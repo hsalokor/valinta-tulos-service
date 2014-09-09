d602205cb7d4a1c811b90c5a935b8ba1bfc04565
Juha Paananen <juha.paananen@gmail.com>
2014-09-09 10:53:18 +0300

    trying to patch a ClassDefNotFound when running in actual Tomcat

--- project/build.scala
+++ project/build.scala
@@ -53,6 +53,7 @@ object ValintaTulosServiceBuild extends Build {
         "org.json4s" %% "json4s-ext" % "3.2.10",
         "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.4.1",
         "org.scalatra.scalate" %% "scalate-core" % "1.7.0",
+        "com.sun.jersey" % "jersey-client" % "1.17.1", // <- patch for transitive dependency problem
         "org.springframework" % "spring-jms" % SpringVersion, // <- patch for spring-core-3.1.3 transitive dep
         "org.springframework" % "spring-core" % SpringVersion,
         "org.springframework" % "spring-context" % SpringVersion,
