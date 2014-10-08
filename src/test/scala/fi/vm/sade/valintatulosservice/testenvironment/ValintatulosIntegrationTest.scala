9dccc28ea5640f6f6ac60664e4cec69183f22ac3
Juha Paananen <juha.paananen@gmail.com>
2014-10-08 14:52:59 +0300

    skip integration test if vars file not found

--- src/test/scala/fi/vm/sade/valintatulosservice/testenvironment/ValintatulosIntegrationTest.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/testenvironment/ValintatulosIntegrationTest.scala
@@ -1,22 +1,28 @@
 package fi.vm.sade.valintatulosservice.testenvironment
 
+import java.io.File
+
 import fi.vm.sade.valintatulosservice.ValintatulosService
 import fi.vm.sade.valintatulosservice.config.AppConfig
-import fi.vm.sade.valintatulosservice.config.AppConfig.AppConfig
 import fi.vm.sade.valintatulosservice.domain.Hakemuksentulos
-import fi.vm.sade.valintatulosservice.hakemus.HakemusRepository
+import org.specs2.execute._
 import org.specs2.mutable.Specification
 
 class ValintatulosIntegrationTest extends Specification {
   "in luokka environment" should {
     "return valintatulos for " in {
-      implicit val appConfig = new AppConfig.LocalTestingWithTemplatedVars("../deploy/vars/environments/ophitest_vars.yml")
+      val varsFile = "../deploy/vars/environments/ophitest_vars.ymlqwer"
+      if (new File(varsFile).exists()) {
+        implicit val appConfig = new AppConfig.LocalTestingWithTemplatedVars(varsFile)
 
-      val valintatulosService = new ValintatulosService()
+        val valintatulosService = new ValintatulosService()
 
-      val tulos: Hakemuksentulos = valintatulosService.hakemuksentulos("1.2.246.562.29.92175749016", "1.2.246.562.11.00000000330").get
+        val tulos: Hakemuksentulos = valintatulosService.hakemuksentulos("1.2.246.562.29.92175749016", "1.2.246.562.11.00000000330").get
 
-      tulos.hakutoiveet.length must_== 2
+        tulos.hakutoiveet.length must_== 2
+      } else {
+        throw new SkipException(Skipped("Variables file not found at " + varsFile))
+      }
     }
   }
 }
