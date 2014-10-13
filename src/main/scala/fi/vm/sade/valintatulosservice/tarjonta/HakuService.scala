853f11b09866f646942b91fe59217a7d6972b567
Juha Paananen <juha.paananen@gmail.com>
2014-10-13 10:51:54 +0300

    koodistoon viittaavat vakiot siirretty properties-tiedostoon

--- src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/HakuService.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/HakuService.scala
@@ -1,6 +1,7 @@
 package fi.vm.sade.valintatulosservice.tarjonta
 
 import fi.vm.sade.valintatulosservice.config.AppConfig.{StubbedExternalDeps, AppConfig}
+import fi.vm.sade.valintatulosservice.config.ApplicationSettings
 import fi.vm.sade.valintatulosservice.http.DefaultHttpClient
 import fi.vm.sade.valintatulosservice.Logging
 
@@ -10,7 +11,7 @@ trait HakuService {
 
 object HakuService {
   def apply(appConfig: AppConfig): HakuService = appConfig match {
-    case _:StubbedExternalDeps => new StubbedHakuService
+    case _:StubbedExternalDeps => new StubbedHakuService(appConfig)
     case _ => new TarjontaHakuService(appConfig)
   }
 }
@@ -20,8 +21,11 @@ trait JsonHakuService {
   import org.json4s.jackson.JsonMethods._
   implicit val formats = DefaultFormats
 
-  protected def parseResponse(response: String): Haku = {
-    (parse(response) \ "result").extract[HakuTarjonnassa]
+  protected def parseResponse(response: String, settings: ApplicationSettings): Haku = {
+    val hakuTarjonnassa = (parse(response) \ "result").extract[HakuTarjonnassa]
+    val korkeakoulu: Boolean = hakuTarjonnassa.kohdejoukkoUri.startsWith(settings.haunKohdejoukkoKorkeakoulu)
+    val yhteishaku: Boolean = hakuTarjonnassa.hakutapaUri.startsWith(settings.hakuTapaYhteishaku)
+    Haku(korkeakoulu, yhteishaku)
   }
 }
 
@@ -33,7 +37,7 @@ class TarjontaHakuService(appConfig: AppConfig) extends HakuService with JsonHak
 
     responseCode match {
       case 200 =>
-        Some(parseResponse(resultString))
+        Some(parseResponse(resultString, appConfig.settings))
       case _ => {
         logger.warn("Get haku from " + url + " failed with status " + responseCode)
         None
@@ -42,21 +46,16 @@ class TarjontaHakuService(appConfig: AppConfig) extends HakuService with JsonHak
   }
 }
 
-class StubbedHakuService extends HakuService with JsonHakuService {
+class StubbedHakuService(appConfig: AppConfig) extends HakuService with JsonHakuService {
   override def getHaku(oid: String) = {
     val fileName = "/fixtures/tarjonta/haku/" + HakuFixtures.activeFixture + ".json"
     Option(getClass.getResourceAsStream(fileName))
       .map(io.Source.fromInputStream(_).mkString)
-      .map(parseResponse(_))
+      .map(parseResponse(_, appConfig.settings))
   }
 }
 
-trait Haku {
-  def korkeakoulu: Boolean
-  def yhteishaku: Boolean
+case class Haku(korkeakoulu: Boolean, yhteishaku: Boolean) {
 }
 
-case class HakuTarjonnassa(oid: String, hakutapaUri: String, hakutyyppiUri: String, kohdejoukkoUri: String) extends Haku {
-  def korkeakoulu: Boolean = kohdejoukkoUri.startsWith("haunkohdejoukko_12#")
-  def yhteishaku: Boolean = hakutapaUri.startsWith("hakutapa_01#")
-}
+case class HakuTarjonnassa(oid: String, hakutapaUri: String, hakutyyppiUri: String, kohdejoukkoUri: String) {}
