7e122d8553e16a28471f52229f0654827b5393ab
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2014-10-22 09:04:27 +0300

    add test case for non-existing haku

--- src/test/scala/fi/vm/sade/valintatulosservice/local/ValintaTulosServletSpec.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/local/ValintaTulosServletSpec.scala
@@ -47,25 +47,22 @@ class ValintaTulosServletSpec extends MutableScalatraSpec with TimeWarp {
         body must_== """[{"hakemusOid":"1.2.246.562.11.00000441369","hakijaOid":"1.2.246.562.24.14229104472","aikataulu":{"vastaanottoEnd":"2100-01-10T12:00:00Z","vastaanottoBufferDays":14},"hakutoiveet":[{"hakukohdeOid":"1.2.246.562.5.72607738902","tarjoajaOid":"1.2.246.562.10.591352080610","valintatapajonoOid":"14090336922663576781797489829886","valintatila":"HYVAKSYTTY","vastaanottotila":"KESKEN","ilmoittautumistila":"EI_TEHTY","vastaanotettavuustila":"VASTAANOTETTAVISSA_SITOVASTI","viimeisinValintatuloksenMuutos":"2014-08-26T19:05:23Z","jonosija":1,"varasijojaKaytetaanAlkaen":"2014-08-26T19:05:23Z","varasijojaTaytetaanAsti":"2014-08-26T19:05:23Z","julkaistavissa":true,"tilanKuvaukset":{}},{"hakukohdeOid":"1.2.246.562.5.16303028779","tarjoajaOid":"1.2.246.562.10.455978782510","valintatapajonoOid":"","valintatila":"PERUUNTUNUT","vastaanottotila":"KESKEN","ilmoittautumistila":"EI_TEHTY","vastaanotettavuustila":"EI_VASTAANOTETTAVISSA","julkaistavissa":true,"tilanKuvaukset":{}}]}]"""
       }
     }
-  }
 
-  /*
-  // FIXME fix loading of haku fixtures
-  "GET /haku/:hakuOid" should {
-    HakuFixtures.activeFixture = "1.2.246.562.5.foo"
     "kun hakua ei löydy" in {
       "404" in {
-        get("/haku/1.2.246.562.5.foo") {
+        HakuFixtures.activeFixture = "notfound"
+          get("/haku/1.2.246.562.5.foo") {
           status must_== 404
           body must_== "Not found"
         }
       }
     }
   }
-  */
+
 
   "POST /haku:hakuId/hakemus/:hakemusId/vastaanota" should {
     "vastaanottaa opiskelupaikan" in {
+      HakuFixtures.activeFixture = HakuFixtures.korkeakouluYhteishaku
       hakemusFixtureImporter.clear.importData("00000441369")
       SijoitteluFixtures.importFixture(appConfig.sijoitteluContext.database, "hyvaksytty-kesken-julkaistavissa.json", true)
       post("/haku/1.2.246.562.5.2013080813081926341928/hakemus/1.2.246.562.11.00000441369/vastaanota",
