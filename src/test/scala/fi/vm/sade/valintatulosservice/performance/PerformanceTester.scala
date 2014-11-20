ea89589616554f33b5714b767961894076076f32
Juha Paananen <juha.paananen@gmail.com>
2014-11-20 16:42:01 +0200

    update PerformanceTester

--- src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTester.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTester.scala
@@ -1,19 +1,19 @@
 package fi.vm.sade.valintatulosservice.performance
 
-import fi.vm.sade.valintatulosservice.ValintatulosService
+import fi.vm.sade.valintatulosservice.{Logging, ValintatulosService}
 import fi.vm.sade.valintatulosservice.config.AppConfig
 import fi.vm.sade.valintatulosservice.config.AppConfig.AppConfig
 import fi.vm.sade.valintatulosservice.fixture.{ExampleFixture, LargerFixture}
 import fi.vm.sade.valintatulosservice.tarjonta.HakuService
 
-object PerformanceTester extends App {
+object PerformanceTester extends App with Logging {
   implicit val appConfig: AppConfig = new AppConfig.IT
   val hakuService = HakuService(appConfig)
   appConfig.start
 
-  new LargerFixture(5, 100000, randomize = true).fixture.apply(appConfig)
+  new LargerFixture(5, 1000, randomize = true).fixture.apply(appConfig)
 
-  println("fixture applied")
+  logger.info("fixture applied")
 
   val haku = hakuService.getHaku("1").get
 
