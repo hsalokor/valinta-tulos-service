cc26e6cc6f35d279ba570f144dfaf367febcaa2b
Juha Paananen <juha.paananen@gmail.com>
2014-11-25 12:23:38 +0200

    poll 5 times in PollerTester, measure performance

--- src/test/scala/fi/vm/sade/valintatulosservice/performance/PollerTester.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/performance/PollerTester.scala
@@ -4,7 +4,7 @@ import fi.vm.sade.valintatulosservice.config.AppConfig
 import fi.vm.sade.valintatulosservice.config.AppConfig.AppConfig
 import fi.vm.sade.valintatulosservice.tarjonta.HakuService
 import fi.vm.sade.valintatulosservice.vastaanottomeili.MailPoller
-import fi.vm.sade.valintatulosservice.{TimeWarp, Logging, ValintatulosService}
+import fi.vm.sade.valintatulosservice.{Timer, TimeWarp, Logging, ValintatulosService}
 
 object PollerTester extends App with Logging with TimeWarp {
   implicit val appConfig: AppConfig = AppConfig.fromSystemProperty
@@ -18,11 +18,16 @@ object PollerTester extends App with Logging with TimeWarp {
   logger.info("Polling...")
 
   withFixedDateTime("22.11.2014 15:00") {
-    val mailables = poller.pollForMailables
-    logger.info("Got stuff")
+    (1 to 5) foreach { num =>
+      val mailables = Timer.timed(0, "pollForMailables") {
+        poller.pollForMailables
+      }
 
-    mailables.foreach { mailStatus =>
-      println(mailStatus.hakemusOid + " -> " + mailStatus.anyMailToBeSent)
+      logger.info("Got mailables")
+
+      mailables.foreach { mailStatus =>
+        println(mailStatus.hakemusOid + " -> " + mailStatus.anyMailToBeSent)
+      }
     }
   }
 }
