dbe8942795285c19765aa192b32d2ab3e796e74f
Juha Paananen <juha.paananen@gmail.com>
2014-11-28 14:27:31 +0200

    add Randomized fixture generator

---
+++ src/main/scala/fi/vm/sade/valintatulosservice/generatedfixtures/RandomizedGeneratedHakuFixture.scala
@@ -0,0 +1,32 @@
+package fi.vm.sade.valintatulosservice.generatedfixtures
+
+import fi.vm.sade.sijoittelu.domain.HakemuksenTila
+
+import scala.util.Random
+
+/**
+ * Luo annetun määrän hakukohteita ja hakemuksia.
+ * Joka toinen on HYVÄKSYTTY, joka toinen HYLÄTTY
+ *
+ * @param hakukohteita
+ * @param hakemuksia
+ * @param hakuOid
+ */
+case class RandomizedGeneratedHakuFixture(hakukohteita: Int, hakemuksia: Int, kohteitaPerHakemus: Int = 5, override val hakuOid: String = "1") extends GeneratedHakuFixture(hakuOid) {
+  val random = new Random()
+  override val hakemukset: List[HakemuksenTulosFixture] = (1 to hakemuksia).map { hakemusNumero =>
+    val hakutoiveet: List[HakemuksenHakukohdeFixture] = (1 to hakukohteita).map { prio =>
+      val hakukohdeNumero = random.nextInt(hakukohteita) + 1
+      val hakukohdeOid = hakukohdeNumero.toString
+      val tarjoajaOid = hakukohdeNumero.toString
+      val totalIndex = (hakemusNumero-1) * hakukohteita + (hakukohdeNumero-1)
+      val tila = if (prio % 2 == 0) { HakemuksenTila.HYVAKSYTTY } else { HakemuksenTila.HYLATTY}
+
+      HakemuksenHakukohdeFixture(tarjoajaOid, hakukohdeOid, jonot = List(ValintatapaJonoFixture(tila)))
+    }.toList
+
+    val hakemusOid = hakuOid + "." + hakemusNumero.toString
+    HakemuksenTulosFixture(hakemusOid, hakutoiveet)
+  }.toList
+}
+
--- src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTester.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/performance/PerformanceTester.scala
@@ -2,16 +2,16 @@ package fi.vm.sade.valintatulosservice.performance
 
 import fi.vm.sade.valintatulosservice.config.AppConfig
 import fi.vm.sade.valintatulosservice.config.AppConfig.AppConfig
-import fi.vm.sade.valintatulosservice.generatedfixtures.{GeneratedFixture, SimpleGeneratedHakuFixture}
+import fi.vm.sade.valintatulosservice.generatedfixtures.{RandomizedGeneratedHakuFixture, GeneratedFixture, SimpleGeneratedHakuFixture}
 import fi.vm.sade.valintatulosservice.tarjonta.HakuService
 import fi.vm.sade.valintatulosservice.{Logging, ValintatulosService}
 
 object PerformanceTester extends App with Logging {
-  implicit val appConfig: AppConfig = new AppConfig.IT
+  implicit val appConfig: AppConfig = new AppConfig.Dev
   val hakuService = HakuService(appConfig)
   appConfig.start
 
-  new GeneratedFixture(new SimpleGeneratedHakuFixture(5, 1000)).apply(appConfig)
+  new GeneratedFixture(new RandomizedGeneratedHakuFixture(100, 1000)).apply(appConfig)
 
   logger.info("fixture applied")
 
