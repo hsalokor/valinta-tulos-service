ca4ba69250fbae35543f9158bdf34751f02770e2
Juha Paananen <juha.paananen@gmail.com>
2015-01-22 13:49:04 +0200

    more tests

--- src/test/scala/fi/vm/sade/valintatulosservice/local/EmailStatusServletSpec.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/local/EmailStatusServletSpec.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.valintatulosservice.local
 import fi.vm.sade.valintatulosservice.vastaanottomeili.{LahetysKuittaus, VastaanotettavuusIlmoitus}
 import fi.vm.sade.valintatulosservice.{ServletSpecification, TimeWarp}
 import org.json4s.jackson.Serialization
+import org.specs2.matcher.MatchResult
 
 class EmailStatusServletSpec extends ServletSpecification with TimeWarp {
   "GET /vastaanottoposti" should {
@@ -10,41 +11,34 @@ class EmailStatusServletSpec extends ServletSpecification with TimeWarp {
       useFixture("hyvaksytty-kesken-julkaistavissa.json", hakemusFixtures = List("00000441369"))
 
       withFixedDateTime("10.10.2014 12:00") {
-        get("vastaanottoposti") {
-          status must_== 200
-          body must_== """[{"hakemusOid":"1.2.246.562.11.00000441369","hakijaOid":"1.2.246.562.24.14229104472","asiointikieli":"FI","etunimi":"Teppo","email":"teppo@testaaja.fi","deadline":"2100-01-10T10:00:00Z","hakukohteet":["1.2.246.562.5.72607738902"]}]"""
-        }
+        expectEmails("""[{"hakemusOid":"1.2.246.562.11.00000441369","hakijaOid":"1.2.246.562.24.14229104472","asiointikieli":"FI","etunimi":"Teppo","email":"teppo@testaaja.fi","deadline":"2100-01-10T10:00:00Z","hakukohteet":["1.2.246.562.5.72607738902"]}]""")
       }
     }
     "Tyhjä lista lähetettävistä sähköposteista, kun ei lähetettävää" in {
       useFixture("hylatty-ei-valintatulosta.json", hakemusFixtures = List("00000441369"))
       withFixedDateTime("10.10.2014 12:00") {
-        get("vastaanottoposti") {
-          status must_== 200
-          body must_== """[]"""
-        }
+        verifyEmptyListOfEmails
       }
     }
 
-    "Kun email-osoite puuttuu" in {
-      useFixture("hyvaksytty-kesken-julkaistavissa.json", hakemusFixtures = List("00000441369-no-email"))
-
+    "Ei lähetetä, jos email-osoite puuttuu" in {
       withFixedDateTime("10.10.2014 12:00") {
-        get("vastaanottoposti") {
-          status must_== 200
-          body must_== """[]"""
-        }
+        useFixture("hyvaksytty-kesken-julkaistavissa.json", hakemusFixtures = List("00000441369-no-email"))
+        verifyEmptyListOfEmails
+      }
+
+      // tarkistetaan, että lähetetään myöhemmin jos email on lisätty
+      withFixedDateTime("12.10.2014 12:00") {
+        hakemusFixtureImporter.importFixture("00000441369")
+        verifyNonEmptyListOfEmails
       }
     }
 
-    "Kun henkilötunnus puuttuu" in {
+    "Ei lähetetä, jos henkilötunnus puuttuu" in {
       useFixture("hyvaksytty-kesken-julkaistavissa.json", hakemusFixtures = List("00000441369-no-hetu"))
 
       withFixedDateTime("10.10.2014 12:00") {
-        get("vastaanottoposti") {
-          status must_== 200
-          body must_== """[]"""
-        }
+        verifyEmptyListOfEmails
       }
     }
   }
@@ -58,12 +52,31 @@ class EmailStatusServletSpec extends ServletSpecification with TimeWarp {
         }
         postJSON("vastaanottoposti", Serialization.write(kuittaukset)) {
           status must_== 200
-          get("vastaanottoposti") {
-            body must_== "[]"
-          }
+          verifyEmptyListOfEmails
         }
       }
     }
   }
 
+
+
+  def verifyEmptyListOfEmails = {
+    verifyEmails { emails => emails must_== "[]"}
+  }
+
+  def verifyNonEmptyListOfEmails = {
+    verifyEmails { emails => emails must_!= "[]"}
+  }
+
+
+  def verifyEmails(check: String => MatchResult[Any]): MatchResult[Any] = {
+    get("vastaanottoposti") {
+      status must_== 200
+      check(body)
+    }
+  }
+
+  def expectEmails(expected: String): MatchResult[Any] = {
+    verifyEmails { emails => emails must_== expected }
+  }
 }
