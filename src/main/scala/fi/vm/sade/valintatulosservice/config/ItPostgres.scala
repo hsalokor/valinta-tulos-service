b2f64b7b7a3217d21297e998dd48d868865fa9c3
Jani Rahkola <jani.rahkola@reaktor.com>
2016-01-27 10:01:42 +0200

    From println to logger

--- src/main/scala/fi/vm/sade/valintatulosservice/config/ItPostgres.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/config/ItPostgres.scala
@@ -4,41 +4,41 @@ import java.io.File
 import java.nio.file.Files
 
 import scala.sys.process.stringToProcess
+import fi.vm.sade.utils.slf4j.Logging
 
-class ItPostgres {
+class ItPostgres extends Logging {
   val dataDirName = "valintarekisteri-it-db"
   val dbName = "valintarekisteri"
   val port = 65432
-  val startTimeoutRetries = 30
-  val startTimeoutPollingIntervalMillis = 100
+  val startRetries = 30
+  val startRetriesIntervalMillis = 100
   private val dataDirFile = new File(dataDirName)
   if (!dataDirFile.isDirectory) {
     Files.createDirectory(dataDirFile.toPath)
   }
   val dataDirPath = dataDirFile.getAbsolutePath
+  logger.info(s"starting postgres to port $port with data directory $dataDirPath")
   s"initdb -D $dataDirPath".!
-  private val dbProcess = s"postgres --config_file=postgresql/postgresql.conf -D $dataDirName -p $port".run()
+  private val dbProcess = s"postgres --config_file=postgresql/postgresql.conf -D $dataDirPath -p $port".run()
 
   private def isAcceptingConnections(): Boolean = {
     s"pg_isready -q -t 1 -h localhost -p $port -d $dbName".! == 0
   }
 
   def start() {
-    var retries = startTimeoutRetries
+    var retries = startRetries
     while (!isAcceptingConnections() && retries > 0) {
       retries -= 1
-      Thread.sleep(startTimeoutPollingIntervalMillis)
+      Thread.sleep(startRetriesIntervalMillis)
     }
     if (!isAcceptingConnections()) {
-      val msg = s"postgres not accepting connections in port $port within $startTimeoutRetries attempts of $startTimeoutPollingIntervalMillis ms intervals"
-      println(msg)
-      throw new RuntimeException(msg)
+      throw new RuntimeException(s"postgres not accepting connections in port $port after $startRetries attempts with $startRetriesIntervalMillis ms intervals")
     }
     s"dropdb -p $port --if-exists $dbName".!
     s"createdb -p $port $dbName".!
   }
 
   def stop() {
-    println("TODO: stop db if needed")
+    logger.warn("TODO: stop db if needed")
   }
 }
