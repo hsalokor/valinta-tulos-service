1ae2728ba7ccb510dc78e980ccb090d568302364
Jani Rahkola <jani.rahkola@reaktor.com>
2016-01-27 11:09:40 +0200

    Stop IT db by sending SIGTERM

--- src/main/scala/fi/vm/sade/valintatulosservice/config/ItPostgres.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/config/ItPostgres.scala
@@ -3,15 +3,17 @@ package fi.vm.sade.valintatulosservice.config
 import java.io.File
 import java.nio.file.Files
 
-import scala.sys.process.stringToProcess
 import fi.vm.sade.utils.slf4j.Logging
+import org.apache.commons.io.FileUtils
+
+import scala.sys.process.stringToProcess
 
 class ItPostgres extends Logging {
   val dataDirName = "valintarekisteri-it-db"
   val dbName = "valintarekisteri"
   val port = 65432
-  val startRetries = 30
-  val startRetriesIntervalMillis = 100
+  val startStopRetries = 30
+  val startStopRetryIntervalMillis = 100
   private val dataDirFile = new File(dataDirName)
   if (!dataDirFile.isDirectory) {
     Files.createDirectory(dataDirFile.toPath)
@@ -25,20 +27,30 @@ class ItPostgres extends Logging {
     s"pg_isready -q -t 1 -h localhost -p $port -d $dbName".! == 0
   }
 
+  private def readPid(): Int = {
+    val pidFile = new File(dataDirFile, "postmaster.pid")
+    FileUtils.readFileToString(pidFile).split("\n")(0).toInt
+  }
+
+  private def tryTimes(times: Int, sleep: Int)(thunk: () => Boolean): Boolean = times match {
+    case n if n < 1 => false
+    case 1 => thunk()
+    case n => thunk() || { Thread.sleep(sleep); tryTimes(n - 1, sleep)(thunk) }
+  }
+
   def start() {
-    var retries = startRetries
-    while (!isAcceptingConnections() && retries > 0) {
-      retries -= 1
-      Thread.sleep(startRetriesIntervalMillis)
-    }
-    if (!isAcceptingConnections()) {
-      throw new RuntimeException(s"postgres not accepting connections in port $port after $startRetries attempts with $startRetriesIntervalMillis ms intervals")
+    if (!tryTimes(startStopRetries, startStopRetryIntervalMillis)(isAcceptingConnections)) {
+      throw new RuntimeException(s"postgres not accepting connections in port $port after $startStopRetries attempts with $startStopRetryIntervalMillis ms intervals")
     }
     s"dropdb -p $port --if-exists $dbName".!
     s"createdb -p $port $dbName".!
   }
 
   def stop() {
-    logger.warn("TODO: stop db if needed")
+    val pid = readPid()
+    s"kill -s SIGTERM $pid".!
+    if (!tryTimes(startStopRetries, startStopRetryIntervalMillis)(() => !isAcceptingConnections())) {
+      logger.error(s"postgres in pid $pid did not stop gracefully after $startStopRetries attempts with $startStopRetryIntervalMillis ms intervals")
+    }
   }
 }
