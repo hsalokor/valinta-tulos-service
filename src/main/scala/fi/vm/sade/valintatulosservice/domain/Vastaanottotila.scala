31248721e1251195d8750cc6a04082e63033653f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-02-09 14:44:00 +0200

    Remove old ValintatulosTila values

--- src/main/scala/fi/vm/sade/valintatulosservice/domain/Vastaanottotila.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/domain/Vastaanottotila.scala
@@ -3,7 +3,7 @@ package fi.vm.sade.valintatulosservice.domain
 object Vastaanottotila extends Enumeration {
   type Vastaanottotila = Value
   val kesken = Value("KESKEN")
-  val vastaanottanut = Value("VASTAANOTTANUT")
+  val vastaanottanut = Value("VASTAANOTTANUT_SITOVASTI")
   val ei_vastaanotettu_määräaikana = Value("EI_VASTAANOTETTU_MAARA_AIKANA")
   val perunut = Value("PERUNUT")
   val peruutettu = Value("PERUUTETTU")
