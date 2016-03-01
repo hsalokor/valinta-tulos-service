ae0916aba3aeb36f13d85a6a9dd12d74778cfea2
Jani Rahkola <jani.rahkola@reaktor.com>
2016-03-01 17:44:14 +0200

    fixup! Revert reversal of deleted reference

--- src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriTools.scala
+++ src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriTools.scala
@@ -3,7 +3,7 @@ package fi.vm.sade.valintatulosservice.valintarekisteri
 import slick.driver.PostgresDriver.api._
 
 object ValintarekisteriTools {
-  private val deleteFromVastaanotot = DBIO.seq(sqlu"delete from deleted_vastaanotot", sqlu"delete from vastaanotot")
+  private val deleteFromVastaanotot = DBIO.seq(sqlu"delete from vastaanotot", sqlu"delete from deleted_vastaanotot")
 
   def deleteAll(db: ValintarekisteriDb): Unit = {
     db.runBlocking(DBIO.seq(
