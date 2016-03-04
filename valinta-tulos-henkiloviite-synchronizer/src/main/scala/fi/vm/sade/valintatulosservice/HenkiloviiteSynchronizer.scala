36f38bd25795745e2d8f6d9030e2f8771cebb70e
Jessika Penttinen <jessika.penttinen@siili.com>
2016-03-04 14:12:37 +0200

    Improve logging

--- valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizer.scala
+++ valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizer.scala
@@ -3,15 +3,21 @@ package fi.vm.sade.valintatulosservice
 import java.util.concurrent.atomic.AtomicBoolean
 
 
+import org.slf4j.LoggerFactory
+
 import scala.util.{Failure, Try, Success}
 
 class HenkiloviiteSynchronizer(henkiloClient: HenkiloviiteClient, db: HenkiloviiteDb) {
 
+  val logger = LoggerFactory.getLogger(classOf[HenkiloviiteSynchronizer])
+
   def startSync(): Try[Unit] = {
     if(startRunning()) {
+      logger.info("Starting henkiloviite sync.")
       new Thread(new HenkiloviiteRunnable).start()
       Success(())
     } else {
+      logger.warn("Attempt to start henkiloviite sync while already running.")
       Failure(new IllegalStateException("Already running."))
     }
   }
@@ -27,9 +33,13 @@ class HenkiloviiteSynchronizer(henkiloClient: HenkiloviiteClient, db: Henkilovii
         henkiloviitteet <- henkiloClient.fetchHenkiloviitteet()
         _ <- db.refresh(henkiloviitteet.toSet)
       } yield {
+        logger.info("Henkiloviite sync finished successfully.")
         stopRunning("OK")
       }).recover{
-        case e => stopRunning(s"Not OK. ${e.getMessage}")
+        case e => {
+          logger.error("Henkiloviite sync failed.", e)
+          stopRunning(s"Not OK. ${e.getMessage}")
+        }
       }
     }
   }
