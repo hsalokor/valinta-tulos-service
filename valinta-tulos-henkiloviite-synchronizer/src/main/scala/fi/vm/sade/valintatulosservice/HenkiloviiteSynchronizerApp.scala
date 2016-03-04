6a7d05522885fb59557ac3a9582af73b29823f79
Jani Rahkola <jani.rahkola@reaktor.com>
2016-03-04 16:17:48 +0200

    Orderly shutdown

--- valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizerApp.scala
+++ valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizerApp.scala
@@ -1,31 +1,45 @@
 package fi.vm.sade.valintatulosservice
 
 import java.io.FileInputStream
+import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
 import java.util.{Calendar, Properties}
-import java.util.concurrent.{TimeUnit, ScheduledThreadPoolExecutor}
-
-import scala.collection.JavaConversions._
 
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{ServletHandler, ServletHolder}
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
 
 object HenkiloviiteSynchronizerApp {
+  val logger = LoggerFactory.getLogger(HenkiloviiteSynchronizerApp.getClass)
+
   def main(args: Array[String]): Unit = {
     val config = readConfig()
     val henkiloviiteClient = new HenkiloviiteClient(config)
     val db = new HenkiloviiteDb(config)
     val synchronizer = new HenkiloviiteSynchronizer(henkiloviiteClient, db)
     val servlet = new HenkiloviiteSynchronizerServlet(synchronizer)
+    val synchronizerScheduler = new ScheduledThreadPoolExecutor(1)
 
-    new ScheduledThreadPoolExecutor(1).scheduleAtFixedRate(synchronizer, hoursForSchedulerStart(config), 24, TimeUnit.HOURS)
+    hoursUntilSchedulerStart(config) match {
+      case Some(delay) =>
+        synchronizerScheduler.scheduleAtFixedRate(synchronizer, delay, 24, TimeUnit.HOURS)
+      case None =>
+        logger.warn("henkiloviite.scheduler.start.hour not given, scheduled synchronization not started.")
+    }
 
     val server = new Server(8080)
     val servletHandler = new ServletHandler
     server.setHandler(servletHandler)
     servletHandler.addServletWithMapping(new ServletHolder(servlet), "/*")
+    Runtime.getRuntime.addShutdownHook(new Thread(new Runnable {
+      override def run(): Unit = {
+        server.stop()
+        synchronizerScheduler.shutdown()
+      }
+    }))
     server.start()
     server.join()
-
   }
 
   private def readConfig(): Properties = {
@@ -41,12 +55,12 @@ object HenkiloviiteSynchronizerApp {
     }
   }
 
-  private def hoursForSchedulerStart(config:Properties):Long = {
-    val hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-    val startHour = config.getProperty("henkiloviite.scheduler.start.hour", "02").toLong
-
-    if(hourOfDay < startHour) startHour - hourOfDay
-    else 24 - hourOfDay + startHour
+  private def hoursUntilSchedulerStart(config:Properties): Option[Long] = {
+    Option(config.getProperty("henkiloviite.scheduler.start.hour")).map(_.toLong).map(startHour => {
+      val hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+      if(hourOfDay < startHour) startHour - hourOfDay
+      else 24 - hourOfDay + startHour
+    })
   }
 
 }
