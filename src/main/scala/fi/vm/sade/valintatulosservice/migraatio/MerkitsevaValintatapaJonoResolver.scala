f64fc1c77f5bc91f06fc5d849031e5a6516bc7af
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-03-18 12:35:15 +0200

    Cache merkitseva jono oid by hakemus+haku

--- src/main/scala/fi/vm/sade/valintatulosservice/migraatio/MerkitsevaValintatapaJonoResolver.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/migraatio/MerkitsevaValintatapaJonoResolver.scala
@@ -6,16 +6,27 @@ import fi.vm.sade.sijoittelu.tulos.service.RaportointiService
 import fi.vm.sade.valintatulosservice.sijoittelu.JonoFinder
 
 class MerkitsevaValintatapaJonoResolver(raportointiService: RaportointiService) {
-  private var latestSijoitteluAjosByHakuOids = Map[String, SijoitteluAjo]()
+  private type HakuOid = String
+  private type HakijaOid = String
+  private type HakemusOid = String
+  private type HakukohdeOid = String
+  private type JonoOid = String
 
-  def hakemuksenKohteidenMerkitsevatJonot(hakuOid: String, hakemusOid: String, hakijaOid: String): Option[List[(String, String)]] = {
-    for (
-      sijoitteluAjo <- findSijoitteluAjo(hakuOid);
-      hakija: HakijaDTO <- Option(raportointiService.hakemus(sijoitteluAjo, hakemusOid))
-    ) yield merkitsevatJonot(hakija)
+  private var latestSijoitteluAjosByHakuOids = Map[HakuOid, SijoitteluAjo]()
+  private var merkitsevatJonoOiditByHakuAndHakemusOids = Map[(HakuOid, HakemusOid), List[(HakukohdeOid, JonoOid)]]()
+
+  def hakemuksenKohteidenMerkitsevatJonot(hakuOid: HakuOid, hakemusOid: HakemusOid, hakijaOid: HakijaOid): Option[List[(HakukohdeOid, JonoOid)]] = {
+    if (merkitsevatJonoOiditByHakuAndHakemusOids.contains((hakuOid, hakemusOid))) {
+      merkitsevatJonoOiditByHakuAndHakemusOids.get((hakuOid, hakemusOid))
+    } else {
+      for (
+        sijoitteluAjo <- findSijoitteluAjo(hakuOid);
+        hakija: HakijaDTO <- Option(raportointiService.hakemus(sijoitteluAjo, hakemusOid))
+      ) yield merkitsevatJonot(hakija, hakuOid, hakemusOid)
+    }
   }
 
-  private def findSijoitteluAjo(hakuOid: String): Option[SijoitteluAjo] = {
+  private def findSijoitteluAjo(hakuOid: HakuOid): Option[SijoitteluAjo] = {
     if (latestSijoitteluAjosByHakuOids.contains(hakuOid)) {
       latestSijoitteluAjosByHakuOids.get(hakuOid)
     } else {
@@ -29,10 +40,12 @@ class MerkitsevaValintatapaJonoResolver(raportointiService: RaportointiService) 
     }
   }
 
-  private def merkitsevatJonot(hakija: HakijaDTO): List[(String, String)] = {
+  private def merkitsevatJonot(hakija: HakijaDTO, hakuOid: HakuOid, hakemusOid: HakemusOid): List[(HakukohdeOid, JonoOid)] = {
     import scala.collection.JavaConversions._
-    hakija.getHakutoiveet.toList.map { hakutoive: HakutoiveDTO =>
+    val merkitsevatJonoOiditHakukohteittain: List[(HakukohdeOid, JonoOid)] = hakija.getHakutoiveet.toList.map { hakutoive: HakutoiveDTO =>
       (hakutoive.getHakukohdeOid, JonoFinder.merkitseväJono(hakutoive).get.getValintatapajonoOid)
     }
+    merkitsevatJonoOiditByHakuAndHakemusOids = merkitsevatJonoOiditByHakuAndHakemusOids + ((hakuOid, hakemusOid) -> merkitsevatJonoOiditHakukohteittain)
+    merkitsevatJonoOiditHakukohteittain
   }
 }
