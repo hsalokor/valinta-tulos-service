4e3a6b9806abd519cfcda5cea36b08bbb6708fae
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-04 18:18:38 +0300

    Improve logging in unauthenticated cases for easier debugging

--- src/main/scala/fi/vm/sade/security/CasLdapFilter.scala
+++ src/main/scala/fi/vm/sade/security/CasLdapFilter.scala
@@ -1,11 +1,11 @@
 package fi.vm.sade.security
 
 import com.typesafe.scalalogging.LazyLogging
+import fi.vm.sade.security.ldap.DirectoryClient
 import fi.vm.sade.utils.cas._
-import fi.vm.sade.security.ldap.{DirectoryClient, LdapClient, LdapConfig}
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.{Unauthorized, ScalatraFilter}
 import org.scalatra.json._
+import org.scalatra.{ScalatraFilter, Unauthorized}
 
 import scala.util.{Failure, Success, Try}
 
@@ -28,9 +28,13 @@ class CasLdapFilter(casClient: CasClient, ldapClient: DirectoryClient, casServic
         Try(casClient.validateServiceTicket(casServiceIdentifier)(ticket).run) match {
           case Success(uid) =>
             ldapClient.findUser(uid) match {
-              case Some(user) if (requiredRoles.forall(user.hasRole(_))) =>
-              // Pass!
-              case _ => halt(Unauthorized("error" -> "LDAP access denied"))
+              case Some(user) if requiredRoles.forall(user.hasRole) => // Pass!
+              case Some(user) =>
+                logger.warn(s"User $user does not have all required roles $requiredRoles")
+                halt(Unauthorized("error" -> "LDAP access denied"))
+              case None =>
+                logger.warn(s"""User "$uid" not found in LDAP""")
+                halt(Unauthorized("error" -> "LDAP access denied"))
             }
           case Failure(t) =>
             logger.warn("Cas ticket rejected", t)
