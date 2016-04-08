a30797f5fdc903f727de4d5f3dffa8c2c17fee5e
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-08 16:47:17 +0300

    Allow using custom serializer

--- src/main/scala/fi/vm/sade/valintatulosservice/json/JsonStreamWriter.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/json/JsonStreamWriter.scala
@@ -6,19 +6,23 @@ import org.json4s.Formats
 
 object JsonStreamWriter {
   val writeSize = 100
-  def writeJsonStream(objects: Iterator[AnyRef], writer: PrintWriter)(implicit formats: Formats): Unit = {
+  def writeJsonStream(objects: Iterator[AnyRef], writer: PrintWriter, render: AnyRef => String): Unit = {
     writer.print("[")
     try {
       objects.zipWithIndex.grouped(writeSize).foreach(_.foreach { case (item, index) =>
         if (index > 0) {
           writer.print(",")
         }
-        writer.print(org.json4s.jackson.Serialization.write(item))
+        writer.print(render(item))
       })
       writer.print("]")
     } catch {
       case t: Throwable => throw new StreamingFailureException(t, s""", {"error": "${t.getMessage}"}] """)
     }
+  }
+
+  def writeJsonStream(objects: Iterator[AnyRef], writer: PrintWriter)(implicit formats: Formats): Unit = {
+    writeJsonStream(objects, writer, o => org.json4s.jackson.Serialization.write(o))
   }
 }
 
