90780ecca0037e2dd265ce3fc2112953ebc54d12
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-12 15:21:04 +0300

    Print some config reading errors to stderr
     specs2 is swallowing them nastily

--- src/main/scala/fi/vm/sade/valintatulosservice/config/ApplicationSettings.scala
+++ src/main/scala/fi/vm/sade/valintatulosservice/config/ApplicationSettings.scala
@@ -8,13 +8,13 @@ import org.apache.commons.lang3.BooleanUtils
 case class ApplicationSettings(config: Config) extends fi.vm.sade.utils.config.ApplicationSettings(config) {
   val hakemusMongoConfig: MongoConfig = getMongoConfig(config.getConfig("hakemus.mongodb"))
   val valintatulosMongoConfig: MongoConfig = getMongoConfig(config.getConfig("sijoittelu-service.mongodb"))
-  val ohjausparametritUrl = config.getString("valinta-tulos-service.ohjausparametrit.url")
-  val tarjontaUrl = config.getString("tarjonta-service.url")
-  val sijoitteluServiceRestUrl = config.getString("sijoittelu-service.rest.url")
+  val ohjausparametritUrl = withConfig(_.getString("valinta-tulos-service.ohjausparametrit.url"))
+  val tarjontaUrl = withConfig(_.getString("tarjonta-service.url"))
+  val sijoitteluServiceRestUrl = withConfig(_.getString("sijoittelu-service.rest.url"))
   val securitySettings = new SecuritySettings(config)
-  val valintaRekisteriDbConfig = config.getConfig("valinta-tulos-service.valintarekisteri.db")
-  val valintaRekisteriEnsikertalaisuusMaxPersonOids = config.getInt("valinta-tulos-service.valintarekisteri.ensikertalaisuus.max.henkilo.oids")
-  val lenientTarjontaDataparsing: Boolean = BooleanUtils.isTrue(config.getBoolean("valinta-tulos-service.parsetarjonta.leniently"))
+  val valintaRekisteriDbConfig = withConfig(_.getConfig("valinta-tulos-service.valintarekisteri.db"))
+  val valintaRekisteriEnsikertalaisuusMaxPersonOids = withConfig(_.getInt("valinta-tulos-service.valintarekisteri.ensikertalaisuus.max.henkilo.oids"))
+  val lenientTarjontaDataparsing: Boolean = BooleanUtils.isTrue(withConfig(_.getBoolean("valinta-tulos-service.parsetarjonta.leniently")))
 
   val ilmoittautuminenEnabled = {
     val value = config.getString("valinta-tulos-service.ilmoittautuminen.enabled")
@@ -23,6 +23,17 @@ case class ApplicationSettings(config: Config) extends fi.vm.sade.utils.config.A
     }
     else {
       false
+    }
+  }
+
+  private def withConfig[T](operation: Config => T): T = {
+    try {
+      operation(config)
+    } catch {
+      case e: Throwable =>
+        System.err.println(s"Cannot instantiate ${classOf[ApplicationSettings]} : ${e.getMessage}")
+        e.printStackTrace()
+        throw e
     }
   }
 }
