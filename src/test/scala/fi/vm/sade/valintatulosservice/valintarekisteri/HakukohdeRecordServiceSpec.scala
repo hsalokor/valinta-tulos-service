e25c827fb3d5e5251b84003f6ffbccdd8b63f665
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-12 10:09:34 +0300

    Add test

---
+++ src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/HakukohdeRecordServiceSpec.scala
@@ -0,0 +1,50 @@
+package fi.vm.sade.valintatulosservice.valintarekisteri
+
+import fi.vm.sade.valintatulosservice.domain.{HakukohdeRecord, Kausi}
+import fi.vm.sade.valintatulosservice.tarjonta._
+import org.junit.runner.RunWith
+import org.specs2.matcher.MustThrownExpectations
+import org.specs2.mock.Mockito
+import org.specs2.mock.mockito.{CalledMatchers, MockitoMatchers, MockitoStubs}
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.Scope
+
+@RunWith(classOf[JUnitRunner])
+class HakukohdeRecordServiceSpec extends Specification with MockitoMatchers with MockitoStubs with CalledMatchers {
+
+  "HakukohdeRecordService" in {
+    "return hakukohde records directly from db when found" in new HakukohdeRecordServiceWithMocks {
+      hakukohdeRepository.findHakukohde(hakukohdeOid) returns Some(hakukohdeRecord)
+      hakukohdeRecordService.getHakukohdeRecord(hakukohdeOid) must_== hakukohdeRecord
+      there was noMoreCallsTo(hakuService)
+    }
+    "invoke tarjonta when hakukohde record is not found" in new HakukohdeRecordServiceWithMocks {
+      hakukohdeRepository.findHakukohde(hakukohdeOid) returns None
+      hakuService.getHakukohde(hakukohdeOid) returns Some(hakukohdeFromTarjonta)
+      hakuService.getHaku(hakuOid) returns Some(hakuFromTarjonta)
+      hakuService.getKoulutus(julkaistuKoulutus.oid) returns Some(julkaistuKoulutus)
+      hakukohdeRecordService.getHakukohdeRecord(hakukohdeOid) must_== hakukohdeRecord
+      one(hakukohdeRepository).storeHakukohde(hakukohdeRecord)
+    }
+  }
+
+  trait HakukohdeRecordServiceWithMocks extends Mockito with Scope with MustThrownExpectations {
+    val hakuService = mock[HakuService]
+    val hakukohdeRepository = mock[HakukohdeRepository]
+    val hakuOid = "1.2.246.562.5.73892938273982732"
+    val hakukohdeOid = "1.2.246.562.5.4890340398"
+    val hakukohdeRecord = HakukohdeRecord(hakukohdeOid, hakuOid = hakuOid,
+      yhdenPaikanSaantoVoimassa = true, kktutkintoonJohtava = true, Kausi("2016S"))
+
+    val luonnosKoulutus = Koulutus("1.2.246.562.17.53513994722", Kausi("2017S"), "LUONNOS")
+    val julkaistuKoulutus = Koulutus("1.2.246.562.17.42423443434", Kausi("2016S"), "JULKAISTU")
+
+    val hakukohdeFromTarjonta = Hakukohde(hakukohdeOid, hakuOid, List(julkaistuKoulutus.oid), "KORKEAKOULUTUS", "TUTKINTO")
+    val hakuFromTarjonta: Haku = Haku(hakuOid, korkeakoulu = true, yhteishaku = true, varsinainenhaku = true,
+      lisähaku = false, käyttääSijoittelua = true, varsinaisenHaunOid = None, sisältyvätHaut = Set(), hakuAjat = Nil,
+      YhdenPaikanSaanto(voimassa = true, "Korkeakoulutus ilman kohdejoukon tarkennetta"))
+
+    val hakukohdeRecordService = new HakukohdeRecordService(hakuService, hakukohdeRepository)
+  }
+}
