b1405193c50df0104173d5b8cfaaecaae4dfffd0
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-20 15:27:07 +0300

    Parametrise env base URL
    And make a bit more verbose to help debugging.

--- get-cas-ticket.bash
+++ get-cas-ticket.bash
@@ -6,20 +6,29 @@ if [[ ! -x `which http` ]]; then
 fi
 
 if [ -z $2 ]; then
-  echo "Usage: $0 <username> <password> [target service URL]"
+  echo "Usage: $0 <username> <password> [virkailija base url] [target service URL]"
   exit 3
 fi
 
 USERNAME=$1
 PASSWORD=$2
+
 if [ -z $3 ]; then
-  SERVICE=https://testi.virkailija.opintopolku.fi/valinta-tulos-service
+  VIRKAILIJA=https://testi.virkailija.opintopolku.fi
 else
-  SERVICE=$3
+  VIRKAILIJA=$3
 fi
 
-echo "Making the TGT request"
-TGT_LOCATION_LINE=`http --pretty none --form --print h POST https://testi.virkailija.opintopolku.fi/cas/v1/tickets username=$USERNAME password=$PASSWORD | grep Location | cut -f 2 -d ' '`
+if [ -z $4 ]; then
+  SERVICE=$VIRKAILIJA/valinta-tulos-service
+else
+  SERVICE=$4
+fi
+
+echo "Making the TGT request with:"
+TGT_REQUEST="http --pretty none --form --print h POST $VIRKAILIJA/cas/v1/tickets username=$USERNAME password=$PASSWORD"
+echo $TGT_REQUEST
+TGT_LOCATION_LINE=`$TGT_REQUEST | grep Location | cut -f 2 -d ' '`
 echo "TGT_LOCATION_LINE: $TGT_LOCATION_LINE"
 TGT_LOCATION=`echo ${TGT_LOCATION_LINE} | awk '{gsub(/[[:cntrl:]]/,"")}1'`
 
