10bd57c895e0cfe0fd295feebf4ef4b024ada23e
Jani Rahkola <jani.rahkola@reaktor.com>
2016-05-10 13:34:01 +0300

    newest_vastaanotot in kauden YPS vastaanotot func

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDb.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDb.scala
@@ -154,6 +154,14 @@ class ValintarekisteriDb(dbConfig: Config) extends ValintarekisteriService with 
       })
   }
 
+  override def findkoulutuksenAlkamiskaudenVastaanottaneetYhdenPaikanSaadoksenPiirissa(kausi: Kausi): Set[VastaanottoRecord] = {
+    runBlocking(
+      sql"""select henkilo, haku_oid, hakukohde, action, ilmoittaja, "timestamp"
+            from newest_vastaanotot
+            where koulutuksen_alkamiskausi = ${kausi.toKausiSpec}
+                and yhden_paikan_saanto_voimassa""".as[VastaanottoRecord]).toSet
+  }
+
   override def store(vastaanottoEvent: VastaanottoEvent, vastaanottoDate: Date) = {
     val VastaanottoEvent(henkiloOid, _, hakukohdeOid, action, ilmoittaja, selite) = vastaanottoEvent
     runBlocking(sqlu"""insert into vastaanotot (hakukohde, henkilo, action, ilmoittaja, selite, timestamp)
@@ -253,19 +261,5 @@ class ValintarekisteriDb(dbConfig: Config) extends ValintarekisteriService with 
                 and vo.deleted is null
             order by vo.henkilo, vo.hakukohde, vo.id desc""".as[VastaanottoRecord])
     vastaanottoRecords.toSet
-  }
-
-  override def findkoulutuksenAlkamiskaudenVastaanottaneetYhdenPaikanSaadoksenPiirissa(kausi: Kausi): Set[VastaanottoRecord] = {
-    runBlocking(
-      sql"""select distinct on (vo.henkilo, vo.hakukohde) vo.henkilo as henkiloOid,  hk.haku_oid as hakuOid, hk.hakukohde_oid as hakukohdeOid,
-                                                          vo.action as action, vo.ilmoittaja as ilmoittaja, vo.timestamp as "timestamp"
-            from vastaanotot vo
-            join hakukohteet hk on hk.hakukohde_oid = vo.hakukohde
-            where hk.koulutuksen_alkamiskausi = ${kausi.toKausiSpec}
-                and hk.yhden_paikan_saanto_voimassa
-                and vo.deleted is null
-            order by vo.henkilo, vo.hakukohde, vo.id desc""".as[VastaanottoRecord]
-        .map(_.filter(r => Set[VastaanottoAction](VastaanotaSitovasti, VastaanotaEhdollisesti).contains(r.action)))
-    ).toSet
   }
 }
