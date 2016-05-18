1bf8de0a4cc6fe8b8f51321dcc9ab1c1d798adc8
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-05-18 15:37:25 +0300

    Fix Vastaanottotila deserialisation
    The fact that both Valintatila and Vastaanottotila Enumeration classes contained a member
    called "KESKEN" caused the vastaanotto DTOs to be deserialised by JSON with the wrong
    enumeration instance... then when comparing the objects in VastaanottoService like this
    
      statesMatchingInexistentActions.contains(virkailijanVastaanotto.tila)
    
    the "KESKEN" value from the wrong enum didn't match the comparison.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/domain/Vastaanottotila.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/domain/Vastaanottotila.scala
@@ -2,17 +2,19 @@ package fi.vm.sade.valintatulosservice.domain
 
 import fi.vm.sade.sijoittelu.domain.ValintatuloksenTila
 
-object Vastaanottotila extends Enumeration {
-  type Vastaanottotila = Value
-  val kesken = Value("KESKEN")
-  val vastaanottanut = Value("VASTAANOTTANUT_SITOVASTI")
-  val ei_vastaanotettu_määräaikana = Value("EI_VASTAANOTETTU_MAARA_AIKANA")
-  val perunut = Value("PERUNUT")
-  val peruutettu = Value("PERUUTETTU")
-  val ottanut_vastaan_toisen_paikan = Value("OTTANUT_VASTAAN_TOISEN_PAIKAN")
-  val ehdollisesti_vastaanottanut = Value("EHDOLLISESTI_VASTAANOTTANUT")
+object Vastaanottotila {
+  type Vastaanottotila = String
+  val kesken: Vastaanottotila = "KESKEN"
+  val vastaanottanut: Vastaanottotila = "VASTAANOTTANUT_SITOVASTI"
+  val ei_vastaanotettu_määräaikana: Vastaanottotila = "EI_VASTAANOTETTU_MAARA_AIKANA"
+  val perunut: Vastaanottotila = "PERUNUT"
+  val peruutettu: Vastaanottotila = "PERUUTETTU"
+  val ottanut_vastaan_toisen_paikan: Vastaanottotila = "OTTANUT_VASTAAN_TOISEN_PAIKAN"
+  val ehdollisesti_vastaanottanut: Vastaanottotila = "EHDOLLISESTI_VASTAANOTTANUT"
+  val values: Set[Vastaanottotila] = Set(kesken, vastaanottanut, ei_vastaanotettu_määräaikana,
+    perunut, peruutettu, ottanut_vastaan_toisen_paikan, ehdollisesti_vastaanottanut)
 
   def matches(vastaanottotila: Vastaanottotila, valintatuloksenTila: ValintatuloksenTila): Boolean = {
-    Vastaanottotila.withName(valintatuloksenTila.name()) == vastaanottotila
+    vastaanottotila == valintatuloksenTila.name()
   }
 }
--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/json/JsonFormats.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/json/JsonFormats.scala
@@ -2,14 +2,14 @@ package fi.vm.sade.valintatulosservice.json
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.vm.sade.utils.json4s.GenericJsonFormats
-import fi.vm.sade.valintatulosservice.{HakijanVastaanottoActionSerializer, VirkailijanVastaanottoActionSerializer}
 import fi.vm.sade.valintatulosservice.domain._
 import fi.vm.sade.valintatulosservice.ensikertalaisuus.EnsikertalaisuusSerializer
+import fi.vm.sade.valintatulosservice.{HakijanVastaanottoActionSerializer, VirkailijanVastaanottoActionSerializer}
 import org.json4s.Formats
 import org.json4s.ext.EnumNameSerializer
 
 object JsonFormats {
-  private val enumSerializers = List(new EnumNameSerializer(Vastaanottotila), new EnumNameSerializer(Ilmoittautumistila), new EnumNameSerializer(Vastaanotettavuustila), new EnumNameSerializer(Valintatila), new EnumNameSerializer(Language))
+  private val enumSerializers = List(new EnumNameSerializer(Ilmoittautumistila), new EnumNameSerializer(Vastaanotettavuustila), new EnumNameSerializer(Valintatila), new EnumNameSerializer(Language))
   val customSerializers = List(new LanguageMapSerializer()) ++ enumSerializers ++ List(new EnsikertalaisuusSerializer,
     new HakijanVastaanottoActionSerializer, new VirkailijanVastaanottoActionSerializer)
   val jsonFormats: Formats = GenericJsonFormats.genericFormats ++ customSerializers
