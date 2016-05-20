53cb2a0f0d3130cff0f330643ceb1dc2bde54953
Jani Rahkola <jani.rahkola@reaktor.com>
2016-05-20 09:58:13 +0300

    Timeout to henkilopalvelu HTTP call

--- valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/henkiloviiteClient.scala
+++ valinta-tulos-henkiloviite-synchronizer/src/main/scala/fi/vm/sade/valintatulosservice/henkiloviiteClient.scala
@@ -1,6 +1,7 @@
 package fi.vm.sade.valintatulosservice
 
 import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
 
 import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
 import org.http4s.{Method, Request, Uri}
@@ -8,6 +9,7 @@ import org.json4s.DefaultReaders.StringReader
 import org.json4s.JsonAST.JValue
 import org.json4s.Reader
 
+import scala.concurrent.duration.Duration
 import scala.util.Try
 import scalaz.concurrent.Task
 
@@ -34,7 +36,7 @@ class HenkiloviiteClient(configuration: AuthenticationConfiguration) {
     Try(client.prepare(request).flatMap {
       case r if 200 == r.status.code => r.as[Array[Henkiloviite]].map(_.toList)
       case r => Task.fail(new RuntimeException("Request " + request + " failed with response " + r))
-    }.run)
+    }.runFor(Duration(1, TimeUnit.MINUTES)))
   }
 
   private def createCasClient(): CasAuthenticatingClient = {
