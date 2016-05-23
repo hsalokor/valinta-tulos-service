7f237cce8ca5f159b2e409886a8e3da6fe95df88
Jani Rahkola <jani.rahkola@reaktor.com>
2016-05-23 13:19:40 +0300

    Rename jar inside deploy zip

--- valinta-tulos-henkiloviite-synchronizer/build_deploy_zip.sh
+++ valinta-tulos-henkiloviite-synchronizer/build_deploy_zip.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
+shopt -s nullglob
 
 SERVICE=valinta-tulos-henkiloviite-synchronizer
 BRANCH=master
 ZIP=${PWD}/target/${SERVICE}_${BRANCH}_$(date +%Y%m%d%H%M%S).zip
 
 cd ./target
-zip "${ZIP}" *jar-with-dependencies.jar
+JAR=( ./*jar-with-dependencies.jar )
+[ ${#JAR[@]} -lt 1 ] && { printf "No runnable jar found\n"; exit 1; }
+[ ${#JAR[@]} -gt 1 ] && { printf "Multiple runnable jars found: %s\n" "${JAR[*]}"; exit 1; }
+cp "${JAR[0]}" "${SERVICE}.jar"
+zip -m "${ZIP}" "${SERVICE}.jar"
 cd -
 cd ./src/main/resources
 zip -r "${ZIP}" oph-configuration
