306de5d2af505c7886f24db807f7e349e259bb67
Jani Rahkola <jani.rahkola@reaktor.com>
2016-05-23 12:32:45 +0300

    Handle wrong number of deploy zips

--- valinta-tulos-henkiloviite-synchronizer/deploy.sh
+++ valinta-tulos-henkiloviite-synchronizer/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
+shopt -s nullglob
 
 SERVICE=valinta-tulos-henkiloviite-synchronizer
 BRANCH=master
 DEPLOY_ENVIRONMENT=$1
-ZIP_PATH=${PWD}/target/${SERVICE}_${BRANCH}_*.zip
-ZIP=$(basename ${ZIP_PATH})
+ZIP_PATH=( ./target/${SERVICE}_${BRANCH}_*.zip )
+[ ${#ZIP_PATH[@]} -lt 1 ] && { printf "No deploy zip found\n"; exit 1; }
+[ ${#ZIP_PATH[@]} -gt 1 ] && { printf "Multiple deploy zips found: %s\n" "${ZIP_PATH[*]}"; exit 1; }
+ZIP=$(basename ${ZIP_PATH[0]})
 KEY_FILE=/home/bamboo/.ssh/id_rsa
 DEPLOY_USER=bamboo
 DEPLOY_HOST=liikuntasali.hard.ware.fi
