c34b13fe838657756cae6402074c368540924905
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-05-27 17:03:19 +0300

    Even more performant newest_vastaanotto_events

---
+++ valinta-tulos-service/src/main/resources/db/migration/V21__more_performant_vastaanotto_views_2.sql
@@ -0,0 +1,18 @@
+create or replace view newest_vastaanotto_events as
+    select distinct on (henkilo, hakukohde)
+    henkilo, haku_oid, hakukohde, action, ilmoittaja, timestamp, selite, kk_tutkintoon_johtava, koulutuksen_alkamiskausi, yhden_paikan_saanto_voimassa, id
+    from (
+        select henkilo, haku_oid, hakukohde, action, ilmoittaja, timestamp, selite, kk_tutkintoon_johtava, koulutuksen_alkamiskausi, yhden_paikan_saanto_voimassa, id
+        from vastaanotot
+            join hakukohteet on hakukohde_oid = vastaanotot.hakukohde
+        where deleted is NULL
+    union
+        select henkiloviitteet.person_oid as henkilo, haku_oid, hakukohde, action, ilmoittaja, timestamp, selite, kk_tutkintoon_johtava, koulutuksen_alkamiskausi, yhden_paikan_saanto_voimassa, id
+        from vastaanotot
+            join hakukohteet on hakukohde_oid = vastaanotot.hakukohde
+            join henkiloviitteet on vastaanotot.henkilo in (henkiloviitteet.linked_oid, henkiloviitteet.person_oid)
+        where deleted is NULL
+    ) as t
+    order by henkilo, hakukohde, id desc;
+
+alter view newest_vastaanotto_events owner to oph;
