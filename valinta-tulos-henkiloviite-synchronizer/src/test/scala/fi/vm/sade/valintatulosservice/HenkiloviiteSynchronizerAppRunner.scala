3844785e0605774490e8fb740f0ab091b56361b9
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-05-31 09:54:09 +0300

    Make properties file overridable, output which is used

--- valinta-tulos-henkiloviite-synchronizer/src/test/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizerAppRunner.scala
+++ valinta-tulos-henkiloviite-synchronizer/src/test/scala/fi/vm/sade/valintatulosservice/HenkiloviiteSynchronizerAppRunner.scala
@@ -2,7 +2,10 @@ package fi.vm.sade.valintatulosservice
 
 object HenkiloviiteSynchronizerAppRunner {
   def main(args: Array[String]): Unit = {
-    System.setProperty("henkiloviite.properties", "src/test/resources/luokka.properties")
+    if (!scala.sys.props.contains("henkiloviite.properties")) {
+      scala.sys.props.put("henkiloviite.properties", "src/test/resources/luokka.properties")
+    }
+    println(s"""Using henkiloviite.properties from ${scala.sys.props.get("henkiloviite.properties").get}""")
     System.setProperty("logback.access", "")
     HenkiloviiteSynchronizerApp.main(args)
   }
