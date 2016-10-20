9f941fc63a7dec7f414391bf0a703961bc15659b
Jessika Penttinen <jessika.penttinen@siili.com>
2016-10-20 15:03:10 +0300

    Siirretty skipped toiseen paikkaan

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
@@ -7,8 +7,8 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class SijoitteluServletSpec extends ServletSpecification {
   "POST /sijoittelu/sijoitteluajo" should {
-    skipped //TODO sijoitteluajon parametrit jää nulliksi + tämä rajapinta poistunee tulevaisuudessa
     "Lisää sijoitteluajon" in {
+      skipped //TODO sijoitteluajon parametrit jää nulliksi + tämä rajapinta poistunee tulevaisuudessa
       useFixture("hyvaksytty-kesken-julkaistavissa.json")
       post("sijoittelu/sijoitteluajo", """{"sijoitteluajoId": 1, "hakuOid": 1, "startMils": 111111, "endMils": 222222}""", Map[String, String]("Content-type" -> "Application/json")) {
         status must_== 200
