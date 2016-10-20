0478a4adcd60aeb7903f0c2c9a0291505ef70d0f
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-10-20 08:49:43 +0300

    Create db function for getting hakija.

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/domain/Sijoittelu.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/domain/Sijoittelu.scala
@@ -3,3 +3,5 @@ package fi.vm.sade.valintatulosservice.domain
 import java.util.Date
 
 case class Sijoitteluajo(sijoitteluajoId:Long, hakuOid:String, startMils:Long, endMils:Long)
+
+case class HakijaRecord(etunimi:String, sukunimi:String, hakemusOid:String, hakijaOid:String)
