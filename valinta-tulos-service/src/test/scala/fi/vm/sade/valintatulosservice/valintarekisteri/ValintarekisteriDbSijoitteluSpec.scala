52d3751db3717e63eecad9028d88edaf7b7c6c56
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-10-31 15:40:56 +0200

    Use real test data in SijoitteluDb spec.

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDbSijoitteluSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDbSijoitteluSpec.scala
@@ -17,6 +17,7 @@ import org.springframework.core.io.ClassPathResource
 import slick.dbio.DBIOAction
 import slick.driver.PostgresDriver.api._
 import slick.jdbc.GetResult
+import scala.collection.JavaConverters._
 
 @RunWith(classOf[JUnitRunner])
 class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with BeforeAfterExample {
@@ -146,77 +147,87 @@ class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with B
       })
       storedHakukohteet.length mustEqual wrapper.hakukohteet.length
     }
-    "get hakiija" in {
-      storeHakijaData()
-//      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-//      singleConnectionValintarekisteriDb.storeSijoitteluajo(wrapper.sijoitteluajo)
-      val res = singleConnectionValintarekisteriDb.getHakija("12340", 222).get
-      res.etunimi mustEqual "Perttu"
+    "get hakija" in {
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getHakija("1.2.246.562.11.00006926939", 1476936450191L).get
+      res.etunimi mustEqual "Semi Testi"
     }
 
     "get hakijan hakutoiveet" in {
-      storeHakijaData()
-      val res = singleConnectionValintarekisteriDb.getHakutoiveet("12340", 222)
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getHakutoiveet("1.2.246.562.11.00006926939", 1476936450191L)
       res.size mustEqual 1
-      res.head.hakutoive mustEqual 9999
-      res.head.jonosijaId mustEqual 333
+      res.head.hakutoive mustEqual 6
+      res.head.valintatuloksenTila mustEqual "Hyvaksytty"
+      res.head.tarjoajaOid mustEqual "1.2.246.562.10.83122281013"
     }
 
     "get hakijan pistetiedot" in {
-      storeHakijaData()
-      val res = singleConnectionValintarekisteriDb.getPistetiedot(List(333))
-      res.size mustEqual 3
-      res.head.arvo mustEqual "Arvo1"
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val jonosijaIds = singleConnectionValintarekisteriDb.getHakutoiveet("1.2.246.562.11.00006926939", 1476936450191L).map(h => h.jonosijaId)
+      val res = singleConnectionValintarekisteriDb.getPistetiedot(jonosijaIds)
+      res.size mustEqual 1
+      res.head.tunniste mustEqual "85e2d263-d57d-46e3-3069-651c733c64d8"
     }
 
     "get latest sijoitteluajoid for haku" in {
-      storeHakijaData()
-      singleConnectionValintarekisteriDb.getLatestSijoitteluajoId(hakuOid).get mustEqual 222
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      singleConnectionValintarekisteriDb.getLatestSijoitteluajoId("1.2.246.562.29.75203638285").get mustEqual 1476936450191L
     }
 
     "get sijoitteluajo" in {
-      storeHakijaData()
-      singleConnectionValintarekisteriDb.getSijoitteluajo(hakuOid, 111).get.sijoitteluajoId mustEqual 111
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      singleConnectionValintarekisteriDb.getSijoitteluajo("1.2.246.562.29.75203638285", 1476936450191L).get.sijoitteluajoId mustEqual 1476936450191L
     }
 
     "get sijoitteluajon hakukohteet" in {
-      storeHakijaData()
-      val res = singleConnectionValintarekisteriDb.getSijoitteluajoHakukohteet(222).get
-      res.map(r => r.oid) mustEqual List(hakukohdeOid, otherHakukohdeOid)
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getSijoitteluajoHakukohteet(1476936450191L).get
+      res.map(r => r.oid) mustEqual List("1.2.246.562.20.26643418986", "1.2.246.562.20.56217166919", "1.2.246.562.20.69766139963")
     }
 
     "get valintatapajonot for sijoitteluajo" in {
-      storeHakijaData()
-      val res = singleConnectionValintarekisteriDb.getValintatapajonot(222).get
-      res.map(r => r.oid) mustEqual List("5.5.555.556", "5.5.555.557")
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getValintatapajonot(1476936450191L).get
+      res.map(r => r.oid) mustEqual List("14538080612623056182813241345174", "14539780970882907815262745035155", "14525090029451152170747426429137")
     }
 
     "get hakemukset for valintatapajono" in {
-      storeHakijaData()
-      val res = singleConnectionValintarekisteriDb.getHakemuksetForValintatapajonos(List("5.5.555.556", "5.5.555.557")).get
-      res.map(r => r.hakemusOid) mustEqual List("12340", "12346", "12347")
-      res.head.hakijaOid mustEqual "54320"
-      res.head.hakemusOid mustEqual "12340"
-      res.head.etunimi mustEqual "Perttu"
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getHakemuksetForValintatapajonos(List("14538080612623056182813241345174", "14539780970882907815262745035155", "14525090029451152170747426429137")).get
+      res.size mustEqual 163
     }
 
     "get tilahistoria for hakemus" in {
-      storeHakijaData()
-      storeTilahistoria()
-      val res = singleConnectionValintarekisteriDb.getHakemuksenTilahistoria("5.5.555.557", "12347")
+      // TODO V
+      skipped("Hakemuksen tilahistorialle ei tehdä vielä mitään -> pitää toteuttaa")
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val res = singleConnectionValintarekisteriDb.getHakemuksenTilahistoria("14538080612623056182813241345174", "1.2.246.562.11.00006926939")
       res.size mustEqual 3
     }
 
     "get hakijaryhmat" in {
-      storeHakijaData()
-      singleConnectionValintarekisteriDb.getHakijaryhmat(222).size mustEqual 2
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).size mustEqual 5
+      singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).last.oid mustEqual "14761056762354411505847130564606"
     }
 
     "get hakijaryhman hakemukset" in {
-      storeHakijaData()
-      singleConnectionValintarekisteriDb.getHakijaryhmanHakemukset(1).size mustEqual 3
+      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
+      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+      val hakijaryhmaId = singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).last.id
+      singleConnectionValintarekisteriDb.getHakijaryhmanHakemukset(hakijaryhmaId).size mustEqual 14
     }
-
   }
 
   def loadSijoitteluFromFixture(fixture: String, path: String = "sijoittelu/"):SijoitteluWrapper = {
@@ -226,8 +237,6 @@ class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with B
     val JArray(sijoittelut) = ( json \ "Sijoittelu" )
     val JArray(sijoitteluajot) = ( sijoittelut(0) \ "sijoitteluajot" )
     val sijoitteluajo:SijoitteluAjo = sijoitteluajot(0).extract[SijoitteluajoWrapper].sijoitteluajo
-
-    import scala.collection.JavaConverters._
 
     val JArray(jsonHakukohteet) = ( json \ "Hakukohde" )
     val hakukohteet:List[Hakukohde] = jsonHakukohteet.map(hakukohdeJson => {
@@ -258,12 +267,14 @@ class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with B
     val JArray(jsonValintatulokset) = (json \ "Valintatulos")
     val valintatulokset:List[Valintatulos] = jsonValintatulokset.map(_.extract[SijoitteluajonValinnantulosWrapper].valintatulos)
 
-    val wrapper:SijoitteluWrapper = SijoitteluWrapper(sijoitteluajo, hakukohteet, valintatulokset)
-    hakukohteet.foreach(h => insertHakukohde(h.getOid))
+    val wrapper:SijoitteluWrapper = SijoitteluWrapper(sijoitteluajo, hakukohteet.filter(h => {
+      h.getSijoitteluajoId.equals(sijoitteluajo.getSijoitteluajoId)
+    }), valintatulokset)
+    hakukohteet.foreach(h => insertHakukohde(h.getOid, sijoitteluajo.getHakuOid))
     wrapper
   }
 
-  def insertHakukohde(hakukohdeOid:String) = {
+  def insertHakukohde(hakukohdeOid:String, hakuOid:String) = {
     singleConnectionValintarekisteriDb.runBlocking(DBIOAction.seq(
       sqlu"""insert into hakukohteet (hakukohde_oid, haku_oid, kk_tutkintoon_johtava, yhden_paikan_saanto_voimassa, koulutuksen_alkamiskausi)
            values ($hakukohdeOid, $hakuOid, true, true, '2015K')"""))
@@ -363,98 +374,6 @@ class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with B
             from pistetiedot p
             inner join jonosijat j on j.id = p.jonosija_id
             where j.hakemus_oid = ${hakemusOid}""".as[Pistetieto])
-  }
-
-  private def storeSijoitteluAjot() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into sijoitteluajot (id, haku_oid, start, "end", erillissijoittelu, valisijoittelu) values (111, ${hakuOid}, ${nowDatetime}, ${nowDatetime}, FALSE, FALSE)""",
-      sqlu"""insert into sijoitteluajot (id, haku_oid, start, "end", erillissijoittelu, valisijoittelu) values (222, ${hakuOid}, ${nowDatetime}, ${nowDatetime}, FALSE, FALSE)"""))
-  }
-
-  private def storeSijoitteluajonHakukohteet() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into sijoitteluajon_hakukohteet (id, sijoitteluajo_id, hakukohde_oid, tarjoaja_oid, kaikki_jonot_sijoiteltu) values (51, 111, ${hakukohdeOid}, '123123', FALSE)""",
-      sqlu"""insert into sijoitteluajon_hakukohteet (id, sijoitteluajo_id, hakukohde_oid, tarjoaja_oid, kaikki_jonot_sijoiteltu) values (52, 222, ${hakukohdeOid}, '123123', FALSE)""",
-      sqlu"""insert into sijoitteluajon_hakukohteet (id, sijoitteluajo_id, hakukohde_oid, tarjoaja_oid, kaikki_jonot_sijoiteltu) values (53, 222, ${otherHakukohdeOid}, '123123', FALSE)"""))
-  }
-
-  private def storeValintatapajonot() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into valintatapajonot (oid, sijoitteluajon_hakukohde_id, nimi) values ('5.5.555.555', 51, 'asd')""",
-      sqlu"""insert into valintatapajonot (oid, sijoitteluajon_hakukohde_id, nimi) values ('5.5.555.556', 52, 'asd')""",
-      sqlu"""insert into valintatapajonot (oid, sijoitteluajon_hakukohde_id, nimi) values ('5.5.555.557', 52, 'asd')"""))
-  }
-
-  private def storeJonosijat() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into jonosijat (id, valintatapajono_oid, sijoitteluajon_hakukohde_id, hakemus_oid, hakija_oid, etunimi, sukunimi, prioriteetti, jonosija)
-             values (333, '5.5.555.556', 52, '12340', '54320', 'Perttu', 'Pikkarainen', 9999, 1)""",
-      sqlu"""insert into jonosijat (id, valintatapajono_oid, sijoitteluajon_hakukohde_id, hakemus_oid, hakija_oid, etunimi, sukunimi, prioriteetti, jonosija)
-             values (334, '5.5.555.556', 52, '12346', '54321', 'Teppo', 'The Great', 9999, 1)""",
-      sqlu"""insert into jonosijat (id, valintatapajono_oid, sijoitteluajon_hakukohde_id, hakemus_oid, hakija_oid, etunimi, sukunimi, prioriteetti, jonosija)
-             values (335, '5.5.555.557', 52, '12347', '54321', 'Teppo', 'The Great', 9999, 1)"""))
-  }
-
-  private def storeValinnantulokset() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into valinnantulokset values (1, ${hakukohdeOid}, '5.5.555.556', '12340', 111, 333, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, NULL)""",
-      sqlu"""insert into valinnantulokset values (2, ${hakukohdeOid}, '5.5.555.556', '12346', 111, 334, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, NULL)""",
-      sqlu"""insert into valinnantulokset values (3, ${hakukohdeOid}, '5.5.555.557', '12347', 111, 335, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, NULL)"""
-    ))
-  }
-
-  private def storePistetiedot() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into pistetiedot values (333, 'Tunniste', 'Arvo1', 'Laskennallinen Arvo 1', 'Osallistuminen 1')""",
-      sqlu"""insert into pistetiedot values (333, 'Tunniste', 'Arvo2', 'Laskennallinen Arvo 2', 'Osallistuminen 2')""",
-      sqlu"""insert into pistetiedot values (333, 'Tunniste', 'Arvo3', 'Laskennallinen Arvo 3', 'Osallistuminen 3')"""))
-  }
-
-  private def storeHakukohteet() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into hakukohteet (hakukohde_oid, haku_oid, kk_tutkintoon_johtava, yhden_paikan_saanto_voimassa, koulutuksen_alkamiskausi)
-             values ($hakukohdeOid, $hakuOid, true, true, '2015K')""",
-      sqlu"""insert into hakukohteet (hakukohde_oid, haku_oid, kk_tutkintoon_johtava, yhden_paikan_saanto_voimassa, koulutuksen_alkamiskausi)
-             values ($otherHakukohdeOid, $otherHakuOid, true, true, '2015S')""",
-      sqlu"""insert into hakukohteet (hakukohde_oid, haku_oid, kk_tutkintoon_johtava, yhden_paikan_saanto_voimassa, koulutuksen_alkamiskausi)
-             values ($otherHakukohdeOidForHakuOid, $hakuOid, true, true, '2015K')"""))
-  }
-
-  private def storeHakijaryhmat() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into hakijaryhmat (id, oid, sijoitteluajon_hakukohde_id, nimi) values (1, '77777', 52, 'Testiryhma')""",
-      sqlu"""insert into hakijaryhmat (id, oid, sijoitteluajon_hakukohde_id, nimi) values (2, '88888', 51, 'Testiryhma')"""))
-  }
-
-  private def storeHakijaryhmanHakemukset() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into hakijaryhman_hakemukset (hakijaryhma_id, hakemus_oid) values (1, '12340')""",
-      sqlu"""insert into hakijaryhman_hakemukset (hakijaryhma_id, hakemus_oid) values (1, '12346')""",
-      sqlu"""insert into hakijaryhman_hakemukset (hakijaryhma_id, hakemus_oid) values (2, '12346')""",
-      sqlu"""insert into hakijaryhman_hakemukset (hakijaryhma_id, hakemus_oid) values (2, '12347')""",
-      sqlu"""insert into hakijaryhman_hakemukset (hakijaryhma_id, hakemus_oid) values (1, '12347')"""))
-  }
-
-  private def storeHakijaData() = {
-    storeHakukohteet()
-    storeSijoitteluAjot()
-    storeSijoitteluajonHakukohteet()
-    storeValintatapajonot()
-    storeJonosijat()
-    storeValinnantulokset()
-    storePistetiedot()
-    storeHakijaryhmat()
-    storeHakijaryhmanHakemukset()
-  }
-
-  private def storeTilahistoria() = {
-    singleConnectionValintarekisteriDb.runBlocking(DBIO.seq(
-      sqlu"""insert into deleted_valinnantulokset values (1, 'testeri', 'huonosti meni', ${nowDatetime})""",
-      sqlu"""insert into deleted_valinnantulokset values (2, 'testeri', 'huonosti meni', ${nowDatetime})""",
-      sqlu"""insert into deleted_valinnantulokset values (3, 'testeri', 'huonosti meni', ${nowDatetime})""",
-      sqlu"""insert into valinnantulokset values (4, ${otherHakukohdeOidForHakuOid}, '5.5.555.557', '12347', 111, 333, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, 1)""",
-      sqlu"""insert into valinnantulokset values (5, ${otherHakukohdeOidForHakuOid}, '5.5.555.557', '12347', 111, 333, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, 2)""",
-      sqlu"""insert into valinnantulokset values (6, ${otherHakukohdeOidForHakuOid}, '5.5.555.557', '12347', 111, 333, 'Varalla', 'hyvaksyttyVarasijalta', 'tsers', TRUE, false, false, false, 'Ilmoittaja', 'Selite', ${nowDatetime}, ${nowDatetime}, 3)"""))
   }
 
   override protected def before: Unit = {
