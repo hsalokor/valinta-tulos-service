97c336cc222685adaf669fa38947f222d2cfd211
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-11-01 10:10:47 +0200

    Separate Sijoittelu read and write tests.

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDbSaveSijoitteluSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ValintarekisteriDbSaveSijoitteluSpec.scala
@@ -1,9 +1,8 @@
 package fi.vm.sade.valintatulosservice.valintarekisteri
 
-import fi.vm.sade.sijoittelu.domain.{Hakemus => SijoitteluHakemus, Valintatulos, Valintatapajono, Hakukohde, SijoitteluAjo, Pistetieto, Hakijaryhma}
 import java.sql.Timestamp
 
-import fi.vm.sade.sijoittelu.domain.{Hakukohde, SijoitteluAjo, Valintatapajono, Valintatulos, Hakemus => SijoitteluHakemus}
+import fi.vm.sade.sijoittelu.domain.{Hakijaryhma, Hakukohde, Pistetieto, SijoitteluAjo, Valintatapajono, Valintatulos, Hakemus => SijoitteluHakemus}
 import fi.vm.sade.valintatulosservice.ITSetup
 import fi.vm.sade.valintatulosservice.domain._
 import org.json4s.JsonAST._
@@ -17,10 +16,11 @@ import org.springframework.core.io.ClassPathResource
 import slick.dbio.DBIOAction
 import slick.driver.PostgresDriver.api._
 import slick.jdbc.GetResult
+
 import scala.collection.JavaConverters._
 
 @RunWith(classOf[JUnitRunner])
-class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with BeforeAfterExample {
+class ValintarekisteriDbSaveSijoitteluSpec extends Specification with ITSetup with BeforeAfterExample {
   sequential
   private val hakuOid = "1.2.246.561.29.00000000001"
   private val henkiloOid = "1.2.246.562.24.00000000001"
@@ -146,87 +146,6 @@ class ValintarekisteriDbSijoitteluSpec extends Specification with ITSetup with B
         storedValintatapajonot.length mustEqual hakukohde.getValintatapajonot.size
       })
       storedHakukohteet.length mustEqual wrapper.hakukohteet.length
-    }
-    "get hakija" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getHakija("1.2.246.562.11.00006926939", 1476936450191L).get
-      res.etunimi mustEqual "Semi Testi"
-    }
-
-    "get hakijan hakutoiveet" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getHakutoiveet("1.2.246.562.11.00006926939", 1476936450191L)
-      res.size mustEqual 1
-      res.head.hakutoive mustEqual 6
-      res.head.valintatuloksenTila mustEqual "Hyvaksytty"
-      res.head.tarjoajaOid mustEqual "1.2.246.562.10.83122281013"
-    }
-
-    "get hakijan pistetiedot" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val jonosijaIds = singleConnectionValintarekisteriDb.getHakutoiveet("1.2.246.562.11.00006926939", 1476936450191L).map(h => h.jonosijaId)
-      val res = singleConnectionValintarekisteriDb.getPistetiedot(jonosijaIds)
-      res.size mustEqual 1
-      res.head.tunniste mustEqual "85e2d263-d57d-46e3-3069-651c733c64d8"
-    }
-
-    "get latest sijoitteluajoid for haku" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      singleConnectionValintarekisteriDb.getLatestSijoitteluajoId("1.2.246.562.29.75203638285").get mustEqual 1476936450191L
-    }
-
-    "get sijoitteluajo" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      singleConnectionValintarekisteriDb.getSijoitteluajo("1.2.246.562.29.75203638285", 1476936450191L).get.sijoitteluajoId mustEqual 1476936450191L
-    }
-
-    "get sijoitteluajon hakukohteet" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getSijoitteluajoHakukohteet(1476936450191L).get
-      res.map(r => r.oid) mustEqual List("1.2.246.562.20.26643418986", "1.2.246.562.20.56217166919", "1.2.246.562.20.69766139963")
-    }
-
-    "get valintatapajonot for sijoitteluajo" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getValintatapajonot(1476936450191L).get
-      res.map(r => r.oid) mustEqual List("14538080612623056182813241345174", "14539780970882907815262745035155", "14525090029451152170747426429137")
-    }
-
-    "get hakemukset for valintatapajono" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getHakemuksetForValintatapajonos(List("14538080612623056182813241345174", "14539780970882907815262745035155", "14525090029451152170747426429137")).get
-      res.size mustEqual 163
-    }
-
-    "get tilahistoria for hakemus" in {
-      // TODO V
-      skipped("Hakemuksen tilahistorialle ei tehdä vielä mitään -> pitää toteuttaa")
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val res = singleConnectionValintarekisteriDb.getHakemuksenTilahistoria("14538080612623056182813241345174", "1.2.246.562.11.00006926939")
-      res.size mustEqual 3
-    }
-
-    "get hakijaryhmat" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).size mustEqual 5
-      singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).last.oid mustEqual "14761056762354411505847130564606"
-    }
-
-    "get hakijaryhman hakemukset" in {
-      val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-      singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
-      val hakijaryhmaId = singleConnectionValintarekisteriDb.getHakijaryhmat(1476936450191L).last.id
-      singleConnectionValintarekisteriDb.getHakijaryhmanHakemukset(hakijaryhmaId).size mustEqual 14
     }
   }
 
