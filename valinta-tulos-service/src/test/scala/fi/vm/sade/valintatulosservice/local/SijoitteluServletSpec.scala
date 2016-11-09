9879dd493fb645ca114cfd9e8c5e77c8cc31e664
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-11-09 10:28:01 +0200

    Fix sijoitteluservlet spec fetch sijoittelu test.

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/SijoitteluServletSpec.scala
@@ -1,30 +1,23 @@
 package fi.vm.sade.valintatulosservice.local
 
 import fi.vm.sade.valintatulosservice._
+import fi.vm.sade.valintatulosservice.valintarekisteri.ValintarekisteriDbTools
+import org.json4s.DefaultFormats
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class SijoitteluServletSpec extends ServletSpecification {
-  "POST /sijoittelu/sijoitteluajo" should {
-    "Lisää sijoitteluajon" in {
-      skipped //TODO sijoitteluajon parametrit jää nulliksi + tämä rajapinta poistunee tulevaisuudessa
-      useFixture("hyvaksytty-kesken-julkaistavissa.json")
-      post("sijoittelu/sijoitteluajo", """{"sijoitteluajoId": 1, "hakuOid": 1, "startMils": 111111, "endMils": 222222}""", Map[String, String]("Content-type" -> "Application/json")) {
-        status must_== 200
-        body must_== ""
-      }
-    }
-  }
+class SijoitteluServletSpec extends ServletSpecification with ValintarekisteriDbTools {
+  override implicit val formats = DefaultFormats ++ List(new NumberLongSerializer, new TasasijasaantoSerializer, new ValinnantilaSerializer, new DateSerializer)
+  step(singleConnectionValintarekisteriDb.storeSijoittelu(loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")))
 
-  "GET /sijoittelu/:hakuOid/sijoitteluajo/:sijoitteluajoOid/hakemus/:hakemusOid" should {
-    "Hakee sijoittelun hakemuksen" in {
-      skipped // TODO data ei mene kantaan?
-      useFixture("hyvaksytty-kesken-julkaistavissa.json")
-      get("sijoittelu/1.2.246.562.29.173465377510/sijoitteluajo/1409055160622/hakemus/1.2.246.562.11.00000877688") {
+  "GET /sijoittelu/:hakuOid/sijoitteluajo/:sijoitteluajoOid" should {
+    "Hakee sijoittelun" in {
+      get("sijoittelu/1.2.246.562.29.75203638285/sijoitteluajo/1476936450191") {
         status must_== 200
-        body must_== ""
+        body.isEmpty mustEqual false
       }
     }
   }
+  step(deleteAll)
 }
