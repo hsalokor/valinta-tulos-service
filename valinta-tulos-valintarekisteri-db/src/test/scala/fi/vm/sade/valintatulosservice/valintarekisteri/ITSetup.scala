4f2252b4241e27c5a0afc9ce3d11476aed9c32d7
Jessika Penttinen <jessika.penttinen@siili.com>
2016-11-09 15:10:35 +0200

    Poistettu turha ITAppConfig

--- valinta-tulos-valintarekisteri-db/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ITSetup.scala
+++ valinta-tulos-valintarekisteri-db/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/ITSetup.scala
@@ -1,10 +1,11 @@
 package fi.vm.sade.valintatulosservice.valintarekisteri
 
 import com.typesafe.config.ConfigValueFactory
+import fi.vm.sade.valintatulosservice.config.ValintarekisteriAppConfig
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.ValintarekisteriDb
 
 trait ITSetup {
-  implicit val appConfig = new ITAppConfig.IT
+  implicit val appConfig = new ValintarekisteriAppConfig.IT
   val dbConfig = appConfig.settings.valintaRekisteriDbConfig
 
   lazy val singleConnectionValintarekisteriDb = new ValintarekisteriDb(
