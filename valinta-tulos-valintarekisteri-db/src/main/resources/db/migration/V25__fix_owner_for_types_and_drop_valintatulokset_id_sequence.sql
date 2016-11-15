2352b20b9664469022c290bda6cee113ca6c7997
Jessika Penttinen <jessika.penttinen@siili.com>
2016-11-15 10:58:22 +0200

    Korjataan tyyppien ownerit tietokannassa ja dropataan turha sequence

---
+++ valinta-tulos-valintarekisteri-db/src/main/resources/db/migration/V25__fix_owner_for_types_and_drop_valintatulokset_id_sequence.sql
@@ -0,0 +1,5 @@
+alter type tasasijasaanto owner to oph;
+alter type valinnantila owner to oph;
+alter type valinnantilanTarkenne owner to oph;
+alter type ilmoittautumistila owner to oph;
+drop sequence valintatulokset_id;
