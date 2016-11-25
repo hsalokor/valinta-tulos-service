d59b73e85a745c922b03aa6986b29113918a83f9
Jessika Penttinen <jessika.penttinen@siili.com>
2016-11-25 12:24:45 +0200

    Lisätty työkalu suorituskyvyn mittaamiseen

--- valinta-tulos-valintarekisteri-db/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/sijoittelu/ValintarekisteriForSijoitteluSpec.scala
+++ valinta-tulos-valintarekisteri-db/src/test/scala/fi/vm/sade/valintatulosservice/valintarekisteri/sijoittelu/ValintarekisteriForSijoitteluSpec.scala
@@ -1,6 +1,7 @@
 package fi.vm.sade.valintatulosservice.valintarekisteri.sijoittelu
 
-import fi.vm.sade.valintatulosservice.valintarekisteri.{ITSetup, ValintarekisteriDbTools}
+import fi.vm.sade.utils.slf4j.Logging
+import fi.vm.sade.valintatulosservice.valintarekisteri.{PerformanceTimer, ITSetup, ValintarekisteriDbTools}
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -10,7 +11,7 @@ import scala.collection.JavaConverters._
 
 
 @RunWith(classOf[JUnitRunner])
-class ValintarekisteriForSijoitteluSpec extends Specification with ITSetup with ValintarekisteriDbTools with BeforeAfterExample {
+class ValintarekisteriForSijoitteluSpec extends Specification with ITSetup with ValintarekisteriDbTools with BeforeAfterExample with Logging with PerformanceTimer {
   sequential
   step(appConfig.start)
   step(deleteAll())
@@ -19,28 +20,28 @@ class ValintarekisteriForSijoitteluSpec extends Specification with ITSetup with 
 
   "SijoitteluajoDTO with sijoitteluajoId should be fetched from database" in {
     val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/")
-    singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+    time("Store sijoittelu") { singleConnectionValintarekisteriDb.storeSijoittelu(wrapper) }
     compareSijoitteluWrapperToDTO(
       wrapper,
-      valintarekisteri.getSijoitteluajo("1.2.246.562.29.75203638285", "1476936450191")
+      time("Get sijoitteluajo") { valintarekisteri.getSijoitteluajo("1.2.246.562.29.75203638285", "1476936450191") }
     )
   }
   "SijoitteluajoDTO with latest sijoitteluajoId should be fetched from database" in {
     val wrapper = loadSijoitteluFromFixture("valintatapajono_hakijaryhma_pistetiedot", "sijoittelu/")
-    singleConnectionValintarekisteriDb.storeSijoittelu(wrapper)
+    time("Store sijoittelu") { singleConnectionValintarekisteriDb.storeSijoittelu(wrapper) }
     compareSijoitteluWrapperToDTO(
       wrapper,
-      valintarekisteri.getSijoitteluajo("korkeakoulu-erillishaku", "latest")
+      time("Get sijoitteluajo") { valintarekisteri.getSijoitteluajo("korkeakoulu-erillishaku", "latest") }
     )
   }
   "Sijoittelu and hakukohteet should be saved in database 1" in {
     val wrapper = loadSijoitteluFromFixture("haku-1.2.246.562.29.75203638285", "QA-import/", false)
-    valintarekisteri.tallennaSijoittelu(wrapper.sijoitteluajo, wrapper.hakukohteet.asJava, wrapper.valintatulokset.asJava)
+    time("Tallenna sijoittelu") { valintarekisteri.tallennaSijoittelu(wrapper.sijoitteluajo, wrapper.hakukohteet.asJava, wrapper.valintatulokset.asJava) }
     assertSijoittelu(wrapper)
   }
   "Sijoittelu and hakukohteet should be saved in database 2" in {
     val wrapper = loadSijoitteluFromFixture("valintatapajono_hakijaryhma_pistetiedot", "sijoittelu/", false)
-    valintarekisteri.tallennaSijoittelu(wrapper.sijoitteluajo, wrapper.hakukohteet.asJava, wrapper.valintatulokset.asJava)
+    time("Tallenna sijoittelu") { valintarekisteri.tallennaSijoittelu(wrapper.sijoitteluajo, wrapper.hakukohteet.asJava, wrapper.valintatulokset.asJava) }
     assertSijoittelu(wrapper)
   }
   "Unknown sijoitteluajo cannot be found" in {
