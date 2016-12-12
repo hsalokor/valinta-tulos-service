5b7945d3bec8735152a85402e735afd76c653ebf
Jani Rahkola <jani.rahkola@reaktor.com>
2016-12-12 19:56:09 +0200

    CAS authenticated valinnan tulos API

---
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/security/AuthenticationFilter.scala
@@ -0,0 +1,17 @@
+package fi.vm.sade.security
+
+import org.scalatra.ScalatraFilter
+
+class AuthenticationFilter(loginUrl: String) extends ScalatraFilter {
+  before() {
+    if (!session.contains("authenticated")) {
+      session += ("authenticated" -> false)
+      if (request.requestMethod == org.scalatra.Get) {
+        session += ("redirect_to" -> requestPath)
+      }
+    }
+    if (!session.as[Boolean]("authenticated")) {
+      redirect(loginUrl)
+    }
+  }
+}
--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/ValintatulosRepository.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/sijoittelu/ValintatulosRepository.scala
@@ -3,14 +3,30 @@ package fi.vm.sade.valintatulosservice.sijoittelu
 import fi.vm.sade.sijoittelu.domain.Valintatulos
 import fi.vm.sade.sijoittelu.tulos.dao.ValintatulosDao
 
+import scala.collection.JavaConversions._
 import scala.util.{Failure, Success, Try}
 
 class ValintatulosNotFoundException(msg: String) extends RuntimeException(msg)
 
 class ValintatulosRepository(dao: ValintatulosDao) {
+  def findValintatulokset(valintatapajonoOid: String): Either[Throwable, Seq[Valintatulos]] = {
+    Try(dao.loadValintatuloksetForValintatapajono(valintatapajonoOid)) match {
+      case Success(valintatulokset) => Right(valintatulokset.toList)
+      case Failure(e) => Left(e)
+    }
+  }
+
+  def modifyValintatulos(valintatapajonoOid: String,
+                         hakemusOid: String,
+                         block: (Valintatulos => Unit)): Either[Throwable, Unit] = {
+    val valintatulos = findValintatulos(valintatapajonoOid, hakemusOid)
+    valintatulos.right.foreach(block)
+    valintatulos.right.flatMap(storeValintatulos)
+  }
+
   def modifyValintatulos(hakukohdeOid: String, valintatapajonoOid: String, hakemusOid: String,
                          block: (Valintatulos => Unit)): Either[Throwable, Unit] = {
-    val valintatulos = getValintatulos(hakukohdeOid, valintatapajonoOid, hakemusOid)
+    val valintatulos = findValintatulos(hakukohdeOid, valintatapajonoOid, hakemusOid)
     valintatulos.right.foreach(block)
     valintatulos.right.flatMap(storeValintatulos)
   }
@@ -27,9 +43,18 @@ class ValintatulosRepository(dao: ValintatulosDao) {
     }
   }
 
-  private def getValintatulos(hakukohdeOid: String,
-                              valintatapajonoOid: String,
-                              hakemusOid: String): Either[Throwable, Valintatulos] = {
+  def findValintatulos(valintatapajonoOid: String,
+                       hakemusOid: String): Either[Throwable, Valintatulos] = {
+    Try(Option(dao.loadValintatulosForValintatapajono(valintatapajonoOid, hakemusOid))) match {
+      case Success(Some(valintatulos)) => Right(valintatulos)
+      case Success(None) => Left(new ValintatulosNotFoundException(s"Valintatulos for hakemus $hakemusOid in valintatapajono $valintatapajonoOid not found"))
+      case Failure(e) => Left(e)
+    }
+  }
+
+  def findValintatulos(hakukohdeOid: String,
+                       valintatapajonoOid: String,
+                       hakemusOid: String): Either[Throwable, Valintatulos] = {
     Try(Option(dao.loadValintatulos(hakukohdeOid, valintatapajonoOid, hakemusOid))) match {
       case Success(Some(valintatulos)) => Right(valintatulos)
       case Success(None) => Left(new ValintatulosNotFoundException(s"Valintatulos for hakemus $hakemusOid in valintatapajono $valintatapajonoOid of hakukohde $hakukohdeOid not found"))
