d403c227aefd07e584e0dbabdd848f053b7f5a27
Jessika Penttinen <jessika.penttinen@siili.com>
2016-12-13 09:52:14 +0200

    Lisätty tilahistorian testaus. Parannettu lokitusta.

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/production/SijoitteluRestTest.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/production/SijoitteluRestTest.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.valintatulosservice.production
 import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
 import fi.vm.sade.utils.http.{DefaultHttpClient, DefaultHttpRequest}
 import fi.vm.sade.utils.slf4j.Logging
+import fi.vm.sade.valintatulosservice.valintarekisteri.PerformanceTimer
 import org.http4s.client.Client
 import org.http4s.{Method, Request, Uri}
 import org.json4s.DefaultFormats
@@ -13,14 +14,15 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import org.specs2.matcher.MatcherMacros
 
-import scalaj.http.Http
+import scala.util.Try
+import scalaj.http.{HttpOptions, Http}
 import scalaz.concurrent.Task
 
 import scala.language.experimental.macros
 
 @Ignore
 @RunWith(classOf[JUnitRunner])
-class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
+class SijoitteluRestTest extends Specification with MatcherMacros with Logging with PerformanceTimer {
   val host = "https://itest-virkailija.oph.ware.fi"
   val cas_user = "xxxx"
   val cas_password = "xxxx"
@@ -29,15 +31,22 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
   //val haku_oid = "1.2.246.562.29.14662042044"
   //val haku_oid = "1.2.246.562.29.95390561488"
 
+  val infoOn = true
+  val debugOn = false
+
+  def info(message:String) = if(infoOn) logger.info(message)
+  def debug(message:String) = if(debugOn) logger.info(message)
+
   implicit val formats = DefaultFormats
 
   val vanhaSijoitteluCasClient = createCasClient("/sijoittelu-service")
 
   "New sijoittelu (valintarekisteri) and old sijoittelu (sijoitteluDb)" should {
     "contain same information" in {
-      val uusiSijoittelu:Sijoitteluajo = get[Sijoitteluajo](getNewSijoittelu)
-      val vanhaSijoittelu = createVanhaSijoitteluajo()
+      val uusiSijoittelu:Sijoitteluajo = time("Create uusi sijoittelu") { get[Sijoitteluajo](getNewSijoittelu) }
+      val vanhaSijoittelu = time("Create vanha sijoittelu") { createVanhaSijoitteluajo() }
 
+      info(s"Sijoittelut valmiina")
       uusiSijoittelu must matchA[Sijoitteluajo]
         .sijoitteluajoId(vanhaSijoittelu.sijoitteluajoId)
         .hakuOid(vanhaSijoittelu.hakuOid)
@@ -46,8 +55,13 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
 
       uusiSijoittelu.hakukohteet.size mustEqual vanhaSijoittelu.hakukohteet.size
 
+      var valintatapajonot = 0
+      var hakemukset = 0
+      var hakijaryhmat = 0
+
+      info(s"Hakukohteita ${uusiSijoittelu.hakukohteet.size}")
       uusiSijoittelu.hakukohteet.foreach(uusiHakukohde => {
-        logger.info(s"Hakukohde ${uusiHakukohde.oid}")
+        debug(s"Hakukohde ${uusiHakukohde.oid}")
         val vanhaHakukohde = vanhaSijoittelu.hakukohteet.find(_.oid.equals(uusiHakukohde.oid)).get
         uusiHakukohde must matchA[Hakukohde]
           .sijoitteluajoId(vanhaHakukohde.sijoitteluajoId)
@@ -57,8 +71,9 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
           .ensikertalaisuusHakijaryhmanAlimmatHyvaksytytPisteet(vanhaHakukohde.ensikertalaisuusHakijaryhmanAlimmatHyvaksytytPisteet)
 
         uusiHakukohde.valintatapajonot.size mustEqual vanhaHakukohde.valintatapajonot.size
+        valintatapajonot = valintatapajonot + uusiHakukohde.valintatapajonot.size
         uusiHakukohde.valintatapajonot.foreach(uusiValintatapajono => {
-          logger.info(s"Valintatapajono ${uusiValintatapajono.oid}")
+          debug(s"Valintatapajono ${uusiValintatapajono.oid}")
           val vanhaValintatapajono = vanhaHakukohde.valintatapajonot.find(_.oid.equals(uusiValintatapajono.oid)).get
           uusiValintatapajono must matchA[Valintatapajono]
             .tasasijasaanto(vanhaValintatapajono.tasasijasaanto)
@@ -81,8 +96,9 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
           uusiValintatapajono.valintaesitysHyvaksytty mustEqual vanhaValintatapajono.valintaesitysHyvaksytty
 
           uusiValintatapajono.hakemukset.size mustEqual vanhaValintatapajono.hakemukset.size
+          hakemukset = hakemukset + uusiValintatapajono.hakemukset.size
           uusiValintatapajono.hakemukset.foreach(uusiHakemus => {
-            logger.info(s"Hakemus ${uusiHakemus.hakemusOid}")
+            debug(s"Hakemus ${uusiHakemus.hakemusOid}")
             val vanhaHakemus = vanhaValintatapajono.hakemukset.find(_.hakemusOid.equals(uusiHakemus.hakemusOid)).get
             uusiHakemus must matchA[Hakemus]
               .hakijaOid(vanhaHakemus.hakijaOid)
@@ -101,7 +117,7 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
               .onkoMuuttunutViimeSijoittelussa(vanhaHakemus.onkoMuuttunutViimeSijoittelussa)
               .siirtynytToisestaValintatapajonosta(vanhaHakemus.siirtynytToisestaValintatapajonosta)
 
-            logger.info(s"Tilankuvaukset ${uusiHakemus.tilanKuvaukset}")
+            debug(s"Tilankuvaukset ${uusiHakemus.tilanKuvaukset}")
 
             uusiHakemus.tilanKuvaukset must matchA[Tilankuvaus]
               .EN(vanhaHakemus.tilanKuvaukset.EN)
@@ -111,7 +127,7 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
             uusiHakemus.pistetiedot.size mustEqual vanhaHakemus.pistetiedot.size
             uusiHakemus.pistetiedot.foreach(uusiPistetieto => {
               val vanhaPistetieto = vanhaHakemus.pistetiedot.find(_.tunniste.equals(uusiPistetieto.tunniste)).get
-              logger.info(s"Pistetieto ${uusiPistetieto.tunniste}")
+              debug(s"Pistetieto ${uusiPistetieto.tunniste}")
               uusiPistetieto must matchA[Pistetieto]
                 .tunniste(vanhaPistetieto.tunniste)
                 .arvo(vanhaPistetieto.arvo)
@@ -120,11 +136,23 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
                 .tyypinKoodiUri(vanhaPistetieto.tyypinKoodiUri)
                 .tilastoidaan(vanhaPistetieto.tilastoidaan)
             })
+
+            debug(s"Tilahistoria ${uusiHakemus.tilaHistoria}")
+
+            uusiHakemus.tilaHistoria.size must be_<=(vanhaHakemus.tilaHistoria.size)
+              for((uusiTilahistoria, i) <- uusiHakemus.tilaHistoria.reverse.zipWithIndex) {
+                val vanhaTilahistoria = vanhaHakemus.tilaHistoria.reverse(i)
+                uusiTilahistoria must matchA[Tilahistoria]
+                  .tila(vanhaTilahistoria.tila)
+                  .luotu(vanhaTilahistoria.luotu)
+              }
+
           })
         })
         uusiHakukohde.hakijaryhmat.size mustEqual vanhaHakukohde.hakijaryhmat.size
+        hakijaryhmat = hakijaryhmat + uusiHakukohde.hakijaryhmat.size
         uusiHakukohde.hakijaryhmat.foreach(uusiHakijaryhma => {
-          logger.info(s"Hakijaryhma ${uusiHakijaryhma.oid}")
+          debug(s"Hakijaryhma ${uusiHakijaryhma.oid}")
           val vanhaHakijaryhma = vanhaHakukohde.hakijaryhmat.find(_.oid.equals(uusiHakijaryhma.oid)).get
           uusiHakijaryhma must matchA[Hakijaryhma]
             .prioriteetti(vanhaHakijaryhma.prioriteetti)
@@ -141,6 +169,11 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
           uusiHakijaryhma.hakemusOid.diff(vanhaHakijaryhma.hakemusOid) mustEqual List()
         })
       })
+
+      info(s"Valintatapajonoja ${valintatapajonot}")
+      info(s"Hakemuksia ${hakemukset}")
+      info(s"Hakijaryhmiä ${hakijaryhmat}")
+
       true must beTrue
     }
   }
@@ -156,10 +189,11 @@ class SijoitteluRestTest extends Specification with MatcherMacros with Logging {
   private def getHakukohde(oid:String) = () => getOld(s"${host}/sijoittelu-service/resources/sijoittelu/${haku_oid}/sijoitteluajo/latest/hakukohde/${oid}")
 
   private def getNewSijoittelu() = {
-    val (_, _, result) = new DefaultHttpRequest(Http(s"${host}/valinta-tulos-service/sijoittelu/${haku_oid}/sijoitteluajo/latest")
-      .method("GET")
-      .options(DefaultHttpClient.defaultOptions)
-      .header("Content-Type", "application/json")).responseWithHeaders()
+    val (_, _, result) = time("Uuden sijoittelun haku") {
+      new DefaultHttpRequest(Http(s"${host}/valinta-tulos-service/sijoittelu/${haku_oid}/sijoitteluajo/latest")
+        .method("GET")
+        .options(Seq(HttpOptions.connTimeout(10000), HttpOptions.readTimeout(120000)))
+        .header("Content-Type", "application/json")).responseWithHeaders() }
     result
   }
 
@@ -207,8 +241,10 @@ case class Hakemus(hakijaOid:Option[String], hakemusOid:String, pisteet:Option[L
                    tila:Option[String], hyvaksyttyHarkinnanvaraisesti:Boolean, varasijanNumero:Option[Long], sijoitteluajoId:Option[Long],
                    hakukohdeOid:Option[String], tarjoajaOid:Option[String], valintatapajonoOid:Option[String],
                    hakuOid:Option[String], onkoMuuttunutViimeSijoittelussa:Boolean, siirtynytToisestaValintatapajonosta:Boolean,
-                   tilanKuvaukset:Tilankuvaus, pistetiedot:List[Pistetieto])
+                   tilanKuvaukset:Tilankuvaus, pistetiedot:List[Pistetieto], tilaHistoria:List[Tilahistoria])
 
 case class Pistetieto(tunniste:String, arvo:String, laskennallinenArvo:String, osallistuminen:String, tyypinKoodiUri:String, tilastoidaan:Boolean)
 
 case class Tilankuvaus(SV:Option[String], FI:Option[String], EN:Option[String])
+
+case class Tilahistoria(tila:String, luotu:Long)
