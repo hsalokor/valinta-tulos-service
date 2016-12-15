7214ef8bfa5671f4be608c69e546083b4f1e8283
Jani Rahkola <jani.rahkola@reaktor.com>
2016-12-15 16:01:40 +0200

    Role based authorization to valinnan tulos API

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/security/Session.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/security/Session.scala
@@ -1,8 +1,19 @@
 package fi.vm.sade.valintatulosservice.security
 
+case class Role(s: String)
+
+object Role {
+  val SIJOITTELU_READ = Role("APP_SIJOITTELU_READ")
+  val SIJOITTELU_READ_UPDATE = Role("APP_SIJOITTELU_READ_UPDATE")
+  val SIJOITTELU_CRUD = Role("APP_SIJOITTELU_CRUD")
+}
+
 sealed trait Session {
+  def hasAnyRole(roles: Set[Role]): Boolean
   def personOid: String
 }
 
 case class ServiceTicket(s: String)
-case class CasSession(casTicket: ServiceTicket, personOid: String, roles: List[String]) extends Session
+case class CasSession(casTicket: ServiceTicket, personOid: String, roles: Set[Role]) extends Session {
+  override def hasAnyRole(roles: Set[Role]): Boolean = this.roles.intersect(roles).nonEmpty
+}
