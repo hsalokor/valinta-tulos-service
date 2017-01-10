6934ec00e67c10c7fe4707963d3066e65f9ca167
Jani Rahkola <jani.rahkola@reaktor.com>
2017-01-10 16:01:21 +0200

    Set query timeout

--- valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/VastaanottoRepository.scala
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/valintarekisteri/db/VastaanottoRepository.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.valintatulosservice.valintarekisteri.db
 import java.util.concurrent.TimeUnit
 
 import slick.dbio._
+import slick.driver.PostgresDriver.api.jdbcActionExtensionMethods
 import slick.driver.PostgresDriver.backend.Database
 
 import scala.concurrent.Await
@@ -10,5 +11,10 @@ import scala.concurrent.duration.Duration
 
 trait VastaanottoRepository {
   val db: Database
-  def runBlocking[R](operations: DBIO[R], timeout: Duration = Duration(20, TimeUnit.SECONDS)) = Await.result(db.run(operations), timeout)
+  def runBlocking[R](operations: DBIO[R], timeout: Duration = Duration(20, TimeUnit.SECONDS)): R = {
+    Await.result(
+      db.run(operations.withStatementParameters(statementInit = st => st.setQueryTimeout(timeout.toSeconds.toInt))),
+      timeout + Duration(1, TimeUnit.SECONDS)
+    )
+  }
 }
