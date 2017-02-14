f45b1fd99700e6dafdcb9909467ec80f4af30bcf
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-02-14 10:31:29 +0200

    Resolve kela koodit koulutuses

--- valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/kela/KelaService.scala
+++ valinta-tulos-service/src/main/scala/fi/vm/sade/valintatulosservice/kela/KelaService.scala
@@ -6,7 +6,7 @@ import java.util.Date
 import fi.vm.sade.valintatulosservice.migraatio.vastaanotot
 import fi.vm.sade.valintatulosservice.migraatio.vastaanotot.{HakijaResolver}
 import fi.vm.sade.valintatulosservice.organisaatio.{Organisaatio, Organisaatiot, OrganisaatioService}
-import fi.vm.sade.valintatulosservice.tarjonta.{Koulutus, Hakukohde, Haku, HakuService}
+import fi.vm.sade.valintatulosservice.tarjonta._
 import fi.vm.sade.valintatulosservice.valintarekisteri.db.{VastaanottoRecord, ValintarekisteriService}
 import fi.vm.sade.valintatulosservice.valintarekisteri.domain._
 import org.scalatra.swagger.Swagger
@@ -14,12 +14,12 @@ import scala.collection.parallel.ParSeq
 import scala.concurrent.{Future, Await}
 import scala.util.Try
 import scala.concurrent.duration._
+import scala.util.matching.Regex
 
 class KelaService(hakijaResolver: HakijaResolver, hakuService: HakuService, organisaatioService: OrganisaatioService, valintarekisteriService: ValintarekisteriService) {
   private val fetchPersonTimeout = 5 seconds
 
-
-  private def convertToVastaanotto(haku: Haku, hakukohde: Hakukohde, organisaatiot: Organisaatiot, koulutuses: Seq[Koulutus], vastaanotto: VastaanottoRecord): fi.vm.sade.valintatulosservice.kela.Vastaanotto = {
+  private def convertToVastaanotto(haku: Haku, hakukohde: Hakukohde, organisaatiot: Organisaatiot, koulutuses: Seq[Koulutus], vastaanotto: VastaanottoRecord): Option[fi.vm.sade.valintatulosservice.kela.Vastaanotto] = {
     def findOppilaitos(o: Organisaatio): Option[String] =
       o.oppilaitosKoodi.orElse(o.children.flatMap(findOppilaitos).headOption)
 
@@ -29,37 +29,34 @@ class KelaService(hakijaResolver: HakijaResolver, hakuService: HakuService, orga
       case _ =>
         throw new RuntimeException(s"Unable to get oppilaitos for tarjoaja ${hakukohde.tarjoajaOids.head}!")
     }
+    val kelaKoulutus: Option[KelaKoulutus] = KelaKoulutus(koulutuses)
+    val kausi = haku.koulutuksenAlkamiskausi.map(kausiToDate)
 
-
-    val kausi = haku.koulutuksenAlkamiskausi.map(kausiToDate).get
-    fi.vm.sade.valintatulosservice.kela.Vastaanotto(
-      tutkintotyyppi = "",
-      organisaatio = hakukohde.tarjoajaOids.head,
-      oppilaitos = oppilaitos,
-      hakukohde = vastaanotto.hakukohdeOid,
-      tutkinnonlaajuus1 = "",
-      tutkinnonlaajuus2 = None,
-      tutkinnontaso = None,
-      vastaaottoaika = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(vastaanotto.timestamp),
-      alkamiskausipvm = kausi)
+    (kelaKoulutus, kausi) match {
+      case (Some(kela), Some(kausi)) =>
+        Some(fi.vm.sade.valintatulosservice.kela.Vastaanotto(
+          tutkintotyyppi = "",
+          organisaatio = hakukohde.tarjoajaOids.head,
+          oppilaitos = oppilaitos,
+          hakukohde = vastaanotto.hakukohdeOid,
+          tutkinnonlaajuus1 = kela.tutkinnonlaajuus1,
+          tutkinnonlaajuus2 = kela.tutkinnonlaajuus2,
+          tutkinnontaso = kela.tutkinnontaso,
+          vastaaottoaika = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(vastaanotto.timestamp),
+          alkamiskausipvm = kausi))
+      case _ =>
+        None
+    }
   }
 
 
 
   def fetchVastaanototForPersonWithHetu(hetu: String, alkaen: Option[Date]): Option[Henkilo] = {
     val henkilo: Option[vastaanotot.Henkilo] = hakijaResolver.findPersonByHetu(hetu, fetchPersonTimeout)
-    // missingHakijaOidResolver.Henkilo("1.2.3.4", hetu, "Joku", "Joku", "Joku","111190")
+
     henkilo match {
       case Some(henkilo) =>
         val vastaanotot = valintarekisteriService.findHenkilonVastaanotot(henkilo.oidHenkilo, alkaen)
-        /* Seq(VastaanottoRecord(
-          henkiloOid = "1.2.3.4",
-          hakuOid = "1.2.246.562.29.458950780910",
-          hakukohdeOid = "1.2.246.562.20.74678758991",
-          action = VastaanotaSitovasti,
-          ilmoittaja = "", timestamp = new Date
-        ))*/
-
 
         Some(fi.vm.sade.valintatulosservice.kela.Henkilo(
           henkilotunnus = henkilo.hetu,
@@ -77,7 +74,7 @@ class KelaService(hakijaResolver: HakijaResolver, hakuService: HakuService, orga
 
   private def fetchDataForVastaanotot(entry: (String, Seq[VastaanottoRecord])): Seq[fi.vm.sade.valintatulosservice.kela.Vastaanotto] = {
     val (hakuOid, vastaanotot) = entry
-    def hakukohdeAndOrganisaatioForVastaanotto(vastaanotto: VastaanottoRecord, haku: Haku): Either[Throwable, fi.vm.sade.valintatulosservice.kela.Vastaanotto] = {
+    def hakukohdeAndOrganisaatioForVastaanotto(vastaanotto: VastaanottoRecord, haku: Haku): Either[Throwable, Option[fi.vm.sade.valintatulosservice.kela.Vastaanotto]] = {
       for(hakukohde <- hakuService.getHakukohde(vastaanotto.hakukohdeOid).right;
           koulutuses <- hakuService.getKoulutuses(hakukohde.hakukohdeKoulutusOids).right;
           organisaatiot <- organisaatioService.hae(hakukohde.tarjoajaOids.head).right) yield convertToVastaanotto(haku, hakukohde, organisaatiot, koulutuses, vastaanotto)
@@ -89,7 +86,7 @@ class KelaService(hakijaResolver: HakijaResolver, hakuService: HakuService, orga
             vastaanotto
           case Left(e) =>
             throw new RuntimeException(s"Unable to get hakukohde or organisaatio! ${e.getMessage}")
-        }).seq
+        }).seq.flatten
       case Left(e) =>
         throw new RuntimeException(s"Unable to get haku ${hakuOid}! ${e.getMessage}")
     }
---
+++ valinta-tulos-valintarekisteri-db/src/main/scala/fi/vm/sade/valintatulosservice/tarjonta/KelaKoulutus.scala
@@ -0,0 +1,79 @@
+package fi.vm.sade.valintatulosservice.tarjonta
+
+import fi.vm.sade.valintatulosservice.tarjonta.KelaKoulutus.{Hammas, Ylempi, Alempi, Lääkis}
+
+trait KelaKoulutus extends KelaLaajuus with KelaTutkinnontaso
+
+trait KelaLaajuus {
+  val tutkinnonlaajuus1: String
+  val tutkinnonlaajuus2: Option[String]
+}
+trait KelaTutkinnontaso {
+  val tutkinnontaso: Option[String]
+}
+private case class MuuTutkinto(val tutkinnontaso: Option[String] = None, val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+private case class AlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("050"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+private case class AlempiYlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("060"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+private case class ErillinenYlempiKKTutkinto(val tutkinnontaso: Option[String] = Some("061"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+private case class LääketieteenLisensiaatti(val tutkinnontaso: Option[String] = Some("070"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+private case class HammaslääketieteenLisensiaatti(val tutkinnontaso: Option[String] = Some("071"), val tutkinnonlaajuus1: String, val tutkinnonlaajuus2: Option[String] = None) extends KelaTutkinnontaso with KelaLaajuus
+
+object KelaKoulutus {
+
+  private trait Taso
+  private case class Lääkis(val laajuusarvo: Option[String]) extends Taso
+  private case class Hammas(val laajuusarvo: Option[String]) extends Taso
+  private case class Alempi(val laajuusarvo: Option[String]) extends Taso
+  private case class Ylempi(val laajuusarvo: Option[String]) extends Taso
+  private case class Muu(val laajuusarvo: Option[String]) extends Taso
+
+  def apply(ks: Seq[Koulutus]): Option[KelaKoulutus] = {
+    ks.flatMap(toTaso).sortBy {
+      case c: Lääkis => 1
+      case d: Hammas => 2
+      case a: Alempi => 3
+      case b: Ylempi => 4
+      case m: Muu => 5
+    } match {
+      case Ylempi(laajuus) :: Nil =>
+        ErillinenYlempiKKTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse(""))
+      case Alempi(laajuus1) :: Ylempi(laajuus2) :: Nil =>
+        AlempiYlempiKKTutkinto(tutkinnonlaajuus1 = laajuus1.getOrElse(""), tutkinnonlaajuus2 = laajuus2)
+      case Alempi(laajuus) :: Nil =>
+        AlempiKKTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse(""))
+      case Lääkis(laajuus) :: _ =>
+        LääketieteenLisensiaatti(tutkinnonlaajuus1 = laajuus.getOrElse(""))
+      case Hammas(laajuus) :: _ =>
+        HammaslääketieteenLisensiaatti(tutkinnonlaajuus1 = laajuus.getOrElse(""))
+      case Muu(laajuus) :: _ =>
+        MuuTutkinto(tutkinnonlaajuus1 = laajuus.getOrElse(""))
+      case _ =>
+        None
+    }
+    None
+  }
+  private def toTaso(k: Koulutus): Option[Taso] = {
+    implicit class Regex(sc: StringContext) {
+      def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+    }
+    val arvo = k.opintojenLaajuusarvo.map(_.arvo)
+    k.koulutuskoodi match {
+      case Some(koodi) =>
+        koodi.arvo match {
+          case r"772101" =>
+            Some(Lääkis(laajuusarvo = arvo))
+          case r"772201" =>
+            Some(Hammas(laajuusarvo = arvo))
+          case r"6.*" =>
+            Some(Alempi(laajuusarvo = arvo))
+          case r"7.*" =>
+            Some(Ylempi(laajuusarvo = arvo))
+          case _ =>
+            None
+        }
+      case _ =>
+        None
+    }
+  }
+
+}
