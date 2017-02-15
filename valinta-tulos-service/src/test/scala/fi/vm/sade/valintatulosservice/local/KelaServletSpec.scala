a18eed379b700705d9df120c66a8df34d52af7dd
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-02-15 12:04:08 +0200

    cas enabled

--- valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/KelaServletSpec.scala
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/local/KelaServletSpec.scala
@@ -6,15 +6,17 @@ import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class KelaServletSpec extends ServletSpecification {
+  val ticket = "mock-ticket-https://itest-virkailija.oph.ware.fi/valinta-tulos-service-testuser"
+
   "POST /cas/kela/vastaanotot/henkilo" should {
     "palauttaa 204 kun henkilöä ei löydy" in {
-      post("cas/kela/vastaanotot/henkilo", "aabbcc-ddd1".getBytes("UTF-8"), Map("Content-type" -> "text/plain")) {
+      post(s"cas/kela/vastaanotot/henkilo?ticket=${ticket}", "aabbcc-ddd1".getBytes("UTF-8"), Map("Content-type" -> "text/plain")) {
         status must_== 204
       }
     }
 
     "palauttaa 200 kun henkilö löytyy" in {
-      post("cas/kela/vastaanotot/henkilo", "face-beef".getBytes("UTF-8"), Map("Content-type" -> "text/plain")) {
+      post(s"cas/kela/vastaanotot/henkilo?ticket=${ticket}", "face-beef".getBytes("UTF-8"), Map("Content-type" -> "text/plain")) {
         status must_== 200
 
         header.get("Content-Type") must_== Some("application/json; charset=UTF-8")
