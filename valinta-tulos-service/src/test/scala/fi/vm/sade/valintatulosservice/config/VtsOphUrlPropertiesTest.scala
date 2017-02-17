128dc893be339aaf1c04d851f5bfbc1c18a5a219
Joni Mäkelä <joni.makela@gofore.com>
2017-02-17 12:28:10 +0200

    Added simple test for VtsOphUrlProperties, currently it simply iterates through all properties and checks that all resolve correctly.

---
+++ valinta-tulos-service/src/test/scala/fi/vm/sade/valintatulosservice/config/VtsOphUrlPropertiesTest.scala
@@ -0,0 +1,28 @@
+package fi.vm.sade.valintatulosservice.config
+
+import scala.collection.JavaConversions._
+import scala.collection.Set
+
+import fi.vm.sade.valintatulosservice.config.VtsAppConfig.VtsAppConfig
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class VtsOphUrlPropertiesTest extends Specification {
+  "Properties" should {
+    "resolve all" in {
+      val appConfig: VtsAppConfig = new VtsAppConfig.Default
+      val urlprops = appConfig.settings.ophUrlProperties
+      val keys = urlprops.config.load().keySet().toSet
+      val fn = {
+        keys.foreach(s => {
+          urlprops.url(s.toString)
+        })
+      }
+      fn must not throwA[RuntimeException] "Evaluation failed, missing property"
+    }
+  }
+
+
+}
